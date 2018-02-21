2f682733ea81e76ef832c11c3b4f7d927fc63335
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-02-21 17:25:17 +0100

    Update door-sensor_gpiozero.py

--- door-sensor_gpiozero.py
+++ door-sensor_gpiozero.py
@@ -10,30 +10,25 @@ led = LED(14)
 def door_action_closed():
     print("The door has been closed!")
     led.source = button.values
-    sleep(0.2)
 
 def door_action_opened():
     print("The door has been opened!")
     led.source = button.values
-    sleep(0.2)
 
 def door_status_open():
     print("The door is opened!")
     led.source = button.values
-    
+
 def door_status_close():
     print("The door is closed!")
     led.source = button.values
 
 if button.value == 0:
-    print("The door is opened!")
-    led.source = button.values
+    door_status_open()
 else:
-    print("The door is closed!")
-    led.source = button.values
+    door_status_close()
 
 button.when_pressed = door_action_closed
 button.when_released = door_action_opened
 
 pause()
-
