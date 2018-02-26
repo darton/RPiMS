f801cac497b9afb58086163a4a783384ce4b28ca
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-02-26 15:01:39 +0100

    Create zabbix_sender.sh

---
+++ zabbix_sender.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+zabbix_serwer=192.168.1.125
+host_ip=`ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
+
+
+case "$1" in
+
+    'info_when_door_is_opened')
+        zabbix_sender -z $zabbix_serwer -p 10051 -s "$host_ip" -k trap -o "The door is opened"
+    ;;
+    'info_when_door_is_closed')
+        zabbix_sender -z $zabbix_serwer -p 10051 -s "$host_ip" -k trap -o "The door is closed"
+    ;;
+    'info_when_door_has_been_opened')
+        zabbix_sender -z $zabbix_serwer -p 10051 -s "$host_ip" -k trap -o "The door has been opened"
+    ;;
+    'info_when_door_has_been_closed')
+        zabbix_sender -z $zabbix_serwer -p 10051 -s "$host_ip" -k trap -o "The door has been closed"
+    ;;
+           *)
+        echo -e "\nUsage: zabbix_sender.sh info_when_door_is_opened|info_when_door_is_closed|info_when_door_has_been_opened|info_when_door_has_been_closed"
+    ;;
+
+esac
