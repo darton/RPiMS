1b5a4854be446166609445be3822ddfab2621adc
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-02-28 17:42:50 +0100

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -10,8 +10,8 @@ raspivid -o - -t 0 -hf -fps 25 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=
 
 function stream_off {
 
-ps ax|grep raspivid | grep -v grep | awk '{print "kill -9 "$1}' |bash
-ps ax|grep \/usr\/bin\/vlc |grep -v grep | awk '{print "kill -9 "$1}'|bash
+pgrep raspivid | awk '{print "kill -9 "$1}' |bash
+pgrep vlc | awk '{print "kill -9 "$1}'|bash
 exit 0
 
 }
