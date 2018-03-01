89c9e1d814fe4709fb354d0bb3934a94118b9d08
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-03-01 08:35:54 +0100

    Update videorecorder.sh

--- videorecorder.sh
+++ videorecorder.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-raspivid -t 5000  -fps 25 -b 6000000 -p 0,0,1920,1080 -o /home/pi/video/pivideo.h264
+video_dir=/home/pi/video
+video_file=pivideo
 
-cd /home/pi/video
+pkill raspivid
 
-MP4Box -add pivideo.h264 pivideo.mp4
+#raspivid -t 5000 -fps 25 -b 6000000  -a 8 -a "Serwerownia_1 %Y-%m-%d %X" -p 0,0,1920,1080 -o $video_dir/$video_file.h264
+raspivid -t 5000 -fps 25 -b 6000000  -a 8 -a "Serwerownia_1 %Y-%m-%d %X" -n -o $video_dir/$video_file.h264
 
-rm pivideo.h264
+cd $video_dir
+
+MP4Box -add $video_file.h264 $video_file.mp4
+
+rm $video_file.h264
+
+exit 0
