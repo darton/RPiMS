87b42d30aa767877654ba920f18aac00a25c8d6d
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-03-29 13:10:15 +0200

    Update videorecorder.sh

--- videorecorder.sh
+++ videorecorder.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#  (C) Copyright 2018 Dariusz Kowalczyk
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License Version 2 as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
 now=$(date +%H%M%S_%m_%d_%Y)
 
 video_dir=/home/pi/video
