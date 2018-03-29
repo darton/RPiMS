453e65f4bb7a2b2dd9748a3e3392afc5a59ad5bc
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-03-29 13:09:40 +0200

    Update door-sensor.py

--- door-sensor.py
+++ door-sensor.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
+#  (C) Copyright 2018 Dariusz Kowalczyk
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License Version 2 as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
 #from picamera import PiCamera
 from gpiozero import LED, Button
 from signal import pause
