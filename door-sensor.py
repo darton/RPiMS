5679bd4d8add80b6347212122a945c7fd4ba55cf
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-07-16 12:04:41 +0200

    Update door-sensor.py

--- door-sensor.py
+++ door-sensor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-#  (C) Copyright 2018 Dariusz Kowalczyk
-#
+#  Author : Dariusz Kowalczyk
+# 
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License Version 2 as
 #  published by the Free Software Foundation.
