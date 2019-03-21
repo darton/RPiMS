43f9817ee086471ea0edb2d4f4f578d610e7fa35
Dariusz Kowalczyk <darton@users.noreply.github.com>
2019-03-21 13:13:20 +0100

    Update door-sensor.py

--- door-sensor.py
+++ door-sensor.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+#
+#  Author : Dariusz Kowalczyk
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License Version 2 as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
 from gpiozero import LED, Button
 from gpiozero.tools import any_values
 from signal import pause
