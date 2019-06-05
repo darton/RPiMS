66cb296bfd5deecb5167251947cbd101de5c81df
Dariusz Kowalczyk <darton@users.noreply.github.com>
2019-06-05 15:23:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,24 +4,29 @@
 <body>
 
 <?php
-   
-   $redis = new Redis();
-   $redis->connect('127.0.0.1', 6379);
-   
-   $temperature = $redis->get('Temperature');
-   $humidity = $redis->get('Humidity');
-   $door1 = $redis->get('Door1');
-   $door2 = $redis->get('Door2');
-   $door3 = $redis->get('Door3');
-   $door4 = $redis->get('Door4');
 
-   print "<p>Temperature: " . number_format($temperature,2) ." °C</p>";
-   print "<p>Humidity: " . number_format($humidity,2) ." %</p>";
+    $redis = new Redis();
+    $redis->connect('127.0.0.1', 6379);
 
-   print "<p>Door1: " . $door1 ."</p>";
-   print "<p>Door2: " . $door2 ."</p>";
-   print "<p>Door3: " . $door3 ."</p>";
-   print "<p>Door4: " . $door4 ."</p>";
+    $location = $redis->get('Location');
+
+    $temperature = $redis->get('Temperature');
+    $humidity = $redis->get('Humidity');
+
+    $door1 = $redis->get('Door1');
+    $door2 = $redis->get('Door2');
+    $door3 = $redis->get('Door3');
+    $door4 = $redis->get('Door4');
+
+    print "<p>Lokalizacja: " . $location ."</p><br>";
+
+    print "<p style='color:blue;'>Temperature: " . number_format($temperature,2) ." °C</p>";
+    print "<p style='color:blue;'>Humidity: " . number_format($humidity,2) ." %</p><br>";
+
+    print "<p style='color:red;'>Door1: " . $door1 ."</p>";
+    print "<p style='color:red;'>Door2: " . $door2 ."</p>";
+    print "<p style='color:red;'>Door3: " . $door3 ."</p>";
+    print "<p style='color:red;'>Door4: " . $door4 ."</p>";
 
 ?>
 
