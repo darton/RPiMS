a99c0b75e08e38aee1f7ea24951b94e7b57d9ed1
Dariusz Kowalczyk <darton@users.noreply.github.com>
2019-06-05 15:41:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,17 +4,14 @@
 <body>
 
 <?php
-
+    $hostname = gethostname();
+    
     $redis = new Redis();
     $redis->connect('127.0.0.1', 6379);
 
     $location = $redis->get('Location');
-    $hostname = gethostname();
-
-
     $temperature = $redis->get('Temperature');
     $humidity = $redis->get('Humidity');
-
     $door1 = $redis->get('Door1');
     $door2 = $redis->get('Door2');
     $door3 = $redis->get('Door3');
