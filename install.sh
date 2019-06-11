e7cbeeeebd645735d28f253a2d04af97f297e40d
Dariusz Kowalczyk <darton@users.noreply.github.com>
2019-06-11 19:19:13 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 
+#!/bin/bash
+
+installdir=/home/pi/RPiMS
+[[ -d $installdir ]] || mkdir -p $installdir
+
+for file in ADHT.py door-sensor.py redis-get.py redis-get-logdata.py stream.sh videorecorder.sh zabbix_sender.sh README.md index.php; do
+curl -sS https://raw.githubusercontent.com/darton/RPiMS/master/$file > $scriptsdir/$file
+done
+
