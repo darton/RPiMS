cac54d16c594182ef10a51b12d641efa710969ed
Dariusz Kowalczyk <darton@users.noreply.github.com>
2019-07-02 09:21:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,28 +7,29 @@
 <body>
 <?php
     $hostname = gethostname();
-    
+
     $redis = new Redis();
     $redis->connect('127.0.0.1', 6379);
 
     $location = $redis->get('Location');
     $temperature = $redis->get('Temperature');
     $humidity = $redis->get('Humidity');
+    $pressure = $redis->get('Pressure');
     $sensor1 = $redis->get('door_sensor_1');
     $sensor2 = $redis->get('door_sensor_2');
     $sensor3 = $redis->get('door_sensor_3');
-    $sensor4 = $redis->get('door_sensor_4');
 
     print "<p>Location: " . $location ."</p>";
     print "<p>Hostname: " . $hostname ."</p><br>";
 
     print "<p style='color:blue;'>Temperature: " . number_format($temperature,1) ." °C</p>";
-    print "<p style='color:blue;'>Humidity: " . number_format($humidity,1) ." %</p><br>";
+    print "<p style='color:blue;'>Humidity...: " . number_format($humidity,1) ." %</p>";
+    print "<p style='color:blue;'>Pressure...: " . number_format($pressure,1) ." hPa</p><br>";
 
     print "<p style='color:red;'>Door 1 : " . $sensor1 ."</p>";
     print "<p style='color:red;'>Door 2 : " . $sensor2 ."</p>";
     print "<p style='color:red;'>Door 3 : " . $sensor3 ."</p>";
-    print "<p style='color:red;'>Door 4 : " . $sensor4 ."</p>";
+
 ?>
 </body>
 </html>
