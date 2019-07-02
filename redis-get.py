27ead9db2406489c7054a4e6582d620f39ee714b
Dariusz Kowalczyk <darton@users.noreply.github.com>
2019-07-02 08:03:42 +0200

    Update redis-get.py

--- redis-get.py
+++ redis-get.py
@@ -6,6 +6,6 @@ redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8",
 
 temperature = redis_db.get('Temperature')
 humidity = redis_db.get('Humidity')
+pressure = redis_db.get('Pressure')
 
-print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(float(temperature), float(humidity)))
-
+print('Temp={0:0.1f}*C  Humidity={1:0.1f}% Pressure={1:0.1f}%'.format(float(temperature), float(humidity), float(pressure)))
