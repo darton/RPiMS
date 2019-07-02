9af524e474f1ce0799e5298195a2039e3bebcf28
Dariusz Kowalczyk <darton@users.noreply.github.com>
2019-07-02 09:08:00 +0200

    Update redis-get.py

--- redis-get.py
+++ redis-get.py
@@ -8,4 +8,4 @@ temperature = redis_db.get('Temperature')
 humidity = redis_db.get('Humidity')
 pressure = redis_db.get('Pressure')
 
-print('Temp={0:0.1f}*C  Humidity={1:0.1f}% Pressure={1:0.1f}%'.format(float(temperature), float(humidity), float(pressure)))
+print('Temp={0:0.1f}*C  Humidity={1:0.1f}%  Pressure={2:0.1f}hPa'.format(float(temperature),float(humidity),float(pressure)))
