be3e5353608baa56e215a71053ae232a5b8b8b6f
Dariusz Kowalczyk <darton@users.noreply.github.com>
2019-07-03 14:13:45 +0200

    Update sensors.py

--- sensors.py
+++ sensors.py
@@ -59,9 +59,6 @@ joystick_right = Button(26)
 MotionSensor_1 = MotionSensor(12)
 
 button_sensor_list = {
-    "door_sensor_1" : door_sensor_1,
-    "door_sensor_2" : door_sensor_2,
-    "door_sensor_3" : door_sensor_3,
     "button_1"      : button1,
     "button_2"      : button2,
     "button_3"      : button3,
@@ -70,6 +67,12 @@ button_sensor_list = {
     "joystick_fire" : joystick_fire,
     "joystick_down" : joystick_down,
     "joystick_right": joystick_right
+}
+
+door_sensor_list = {
+    "door_sensor_1" : door_sensor_1,
+    "door_sensor_2" : door_sensor_2,
+    "door_sensor_3" : door_sensor_3
 }
 
 motion_sensor_list = {
@@ -195,8 +198,8 @@ def motion_sensor_when_no_motion(ms_id):
         subprocess.call("/home/pi/scripts/RPiMS/stream.sh start", shell=True)
 
 def sensors_read_once():
-    for s in button_sensor_list:
-        if button_sensor_list[s].value == 0:
+    for s in door_sensor_list:
+        if door_sensor_list[s].value == 0:
             door_status_open(s)
         else:
             door_status_close(s)
@@ -208,9 +211,9 @@ sensors_read_once()
 # --- Main program ---
 
 if use_door_sensor is 'yes' :
-    for s in button_sensor_list:
-            button_sensor_list[s].when_pressed = lambda s=s : door_action_closed(s)
-            button_sensor_list[s].when_released = lambda s=s : door_action_opened(s)
+    for s in door_sensor_list:
+            door_sensor_list[s].when_pressed = lambda s=s : door_action_closed(s)
+            door_sensor_list[s].when_released = lambda s=s : door_action_opened(s)
 
 if use_motion_sensor is 'yes' :
     for s in motion_sensor_list:
