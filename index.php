e528daa6100bb644e90ab1d9ca8e09310e129202
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-02-10 15:54:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,25 +10,26 @@
 
     $redis = new Redis();
     $redis->connect('127.0.0.1', 6379);
-
     $location = $redis->get('Location');
     $temperature = $redis->get('Temperature');
     $humidity = $redis->get('Humidity');
     $pressure = $redis->get('Pressure');
     $sensor1 = $redis->get('door_sensor_1');
     $sensor2 = $redis->get('door_sensor_2');
-    $sensor3 = $redis->get('door_sensor_3');
+    $CPUtemp = $redis->get('CPUtemperature');
 
-    print "<p>Location: " . $location ."</p>";
-    print "<p>Hostname: " . $hostname ."</p><br>";
 
-    print "<p style='color:blue;'>Temperature: " . number_format($temperature,1) ." °C</p>";
-    print "<p style='color:blue;'>Humidity...: " . number_format($humidity,1) ." %</p>";
-    print "<p style='color:blue;'>Pressure...: " . number_format($pressure,1) ." hPa</p><br>";
+    print "<p style='color:magenta;'>Location: " . $location ."</p>";
+    print "<p style='color:magenta;'>Hostname: " . $hostname ."</p><br>";
 
-    print "<p style='color:red;'>Door 1 : " . $sensor1 ."</p>";
-    print "<p style='color:red;'>Door 2 : " . $sensor2 ."</p>";
-    print "<p style='color:red;'>Door 3 : " . $sensor3 ."</p>";
+    print "<p style='color:red;'>Air Temperature: " . number_format($temperature,1) ." °C</p>";
+    print "<p style='color:blue;'>Air Humidity...: " . number_format($humidity,1) ." %</p>";
+    print "<p style='color:green;'>Air Pressure...: " . number_format($pressure,1) ." hPa</p><br>";
+
+    print "<p style='color:brown;'>Door 1 : " . $sensor1 ."</p>";
+    print "<p style='color:brown;'>Door 2 : " . $sensor2 ."</p><br>";
+
+    print "<p style='color:red;'>CPU temperature : " . $CPUtemp ."</p>";
 
 ?>
 </body>
