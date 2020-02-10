6d8e320d2f7b02e551696fd36bcc15a0d860e4b8
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-02-10 11:06:22 +0100

    Update cputemp.py

--- cputemp.py
+++ cputemp.py
@@ -3,6 +3,8 @@
 import redis
 from gpiozero import CPUTemperature
 
+redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
 cpu = CPUTemperature()
 #print('CPU temperature: {}C'.format(cpu.temperature))
+redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
 redis_db.set('CPUtemperature', cpu.temperature)
