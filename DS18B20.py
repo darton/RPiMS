3a7e116b985f59fec5b37b550f41b5cf74d82cc2
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-02-13 13:29:18 +0100

    Update DS18B20.py

--- DS18B20.py
+++ DS18B20.py
@@ -5,8 +5,6 @@
 import w1thermsensor
 import redis
 
-redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
-
 
 def sensor_lock(lock_status):
     redis_db.set('BME280_sensor_in_use', str(lock_status))
@@ -19,6 +17,7 @@ def wrtite_sensor_data_to_db():
     print('DS18B20 Temperature: {0:0.2f}C'.format(ds18b20))
 
 
+redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
 sensor_status=redis_db.get('DS18B20_sensor_in_use')
 
 if sensor_status is None:
