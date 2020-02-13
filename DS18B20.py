e6527b0f3809aa7308b4e90ef724f073007d05ca
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-02-13 12:37:07 +0100

    Update DS18B20.py

--- DS18B20.py
+++ DS18B20.py
@@ -5,9 +5,23 @@
 import w1thermsensor
 import redis
 
-sensor = w1thermsensor.W1ThermSensor()
-ds18b20 = sensor.get_temperature()
-print('DS18B20 temperature: {}C'.format(ds18b20))
-
 redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
-redis_db.set('Temperature', ds18b20)
+sensor_status=redis_db.get('DS18B20_sensor_in_use')
+
+if sensor_status is None:
+    redis_db.set('DS18B20_sensor_in_use', '0')
+    sensor_status=redis_db.get('DS18B20_sensor_in_use')
+
+if str(sensor_status) is '0' :
+    redis_db.set('DS18B20_sensor_in_use', '1')
+
+    sensor = w1thermsensor.W1ThermSensor()
+    ds18b20 = sensor.get_temperature()
+    
+    redis_db.set('Temperature', ds18b20)
+    print('DS18B20 Temperature: {0:0.2f}C'.format(ds18b20))
+    
+    redis_db.set('DS18B20_sensor_in_use', '0')
+
+else:
+    print('The sensor is in use, please try again later')
