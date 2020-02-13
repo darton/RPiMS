43c85a03f0d593e44680a54ac106dd017c7fbfd6
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-02-13 13:12:12 +0100

    Update BME280.py

--- BME280.py
+++ BME280.py
@@ -13,14 +13,12 @@ bus = smbus2.SMBus(port)
 
 redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
 
-sensor_status=redis_db.get('BME280_sensor_in_use')
 
-if sensor_status is None:
-    redis_db.set('BME280_sensor_in_use', '0')
-    sensor_status=redis_db.get('BME280_sensor_in_use')
+def sensor_lock(lock_status):
+    redis_db.set('BME280_sensor_in_use', str(lock_status))
 
-if str(sensor_status) is '0' :
-    redis_db.set('BME280_sensor_in_use', '1')
+
+def wrtite_sensor_data_to_db():
 
     calibration_params = bme280.load_calibration_params(bus, address)
     data = bme280.sample(bus, address, calibration_params)
@@ -28,11 +26,23 @@ if str(sensor_status) is '0' :
     redis_db.set('Humidity', data.humidity)
     redis_db.set('Temperature', data.temperature)
     redis_db.set('Pressure', data.pressure)
-    redis_db.set('BME280_sensor_in_use', '0')
 
     print('Humidity: {0:0.0f} %'.format(data.humidity))
     print('Temperature: {0:0.1f} C'.format(data.temperature))
     print('Pressure: {0:0.0f} hPa'.format(data.pressure))
 
+
+sensor_status=redis_db.get('BME280_sensor_in_use')
+
+if  sensor_status is None:
+    sensor_lock(1)
+    wrtite_sensor_data_to_db()
+    sensor_lock(0)
+
+elif str(sensor_status) is '0' :
+     sensor_lock(1)
+     wrtite_sensor_data_to_db()
+     sensor_lock(0)
 else:
     print('The sensor is in use, please try again later')
+
