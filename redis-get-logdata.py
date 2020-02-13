e95d50de85abec25ba1caac3ac13f5829206d46a
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-02-13 14:33:39 +0100

    Update redis-get-logdata.py

--- redis-get-logdata.py
+++ redis-get-logdata.py
@@ -2,13 +2,18 @@
 
 import redis
 import datetime
+from time import sleep
+from pid import PidFile
 
-redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
+with PidFile(piddir='/tmp/'):
 
-temperature = redis_db.get('Temperature')
-humidity = redis_db.get('Humidity')
-pressure = redis_db.get('Pressure')
 
-f = open("/var/tmp/sensor.log", "a")
-f.write( str(datetime.datetime.now()) + ' Temp={0:0.1f}*C Humidity={1:0.1f}% Pressure={2:0.1f}hPa' .format(float(temperature), float(humidity), float(pressure) )  + '\n' )
-f.close()
+    redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
+    sleep(2)
+    temperature = redis_db.get('Temperature')
+    humidity = redis_db.get('Humidity')
+    pressure = redis_db.get('Pressure')
+
+    f = open("/var/tmp/sensor.log", "a")
+    f.write( str(datetime.datetime.now()) + ' Temp={0:0.1f}*C Humidity={1:0.1f}% Pressure={2:0.1f}hPa' .format(float(temperature), float(humidity), float(pressure) )  + '\n' )
+    f.close()
