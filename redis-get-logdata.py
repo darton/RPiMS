41e002364d656cfcd87950d19b977534c57404c9
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-02-13 13:50:49 +0100

    Update redis-get-logdata.py

--- redis-get-logdata.py
+++ redis-get-logdata.py
@@ -3,14 +3,12 @@
 import redis
 import datetime
 
-# -*- coding:utf-8 -*-
-
 redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
 
 temperature = redis_db.get('Temperature')
 humidity = redis_db.get('Humidity')
+pressure = redis_db.get('Pressure')
 
-f = open("/var/tmp/dht22.log", "a")
-f.write( str(datetime.datetime.now()) + ' Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(float(temperature), float(humidity))  + '\n' )
+f = open("/var/tmp/sensor.log", "a")
+f.write( str(datetime.datetime.now()) + ' Temp={0:0.1f}*C  Humidity={1:0.1f}% Pressure={2:0.1f}hPa'  .format(float(temperature), float(humidity), float(pressure) )  + '\n' )
 f.close()
-
