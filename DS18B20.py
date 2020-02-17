54f7332b0681deb9394e4fb9d6eaf8b9cd88b014
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-02-17 10:22:14 +0100

    Update DS18B20.py

--- DS18B20.py
+++ DS18B20.py
@@ -9,12 +9,12 @@ import redis
 def sensor_lock(lock_status):
     redis_db.set('DS18B20_sensor_in_use', str(lock_status))
 
-def wrtite_sensor_data_to_db():
+def write_sensor_data_to_db():
     sensor = W1ThermSensor()
     redis_db.set('Temperature', sensor.get_temperature())
     print('DS18B20 Temperature: {0:0.2f}C'.format(sensor.get_temperature()))
 
-def wrtite_sensors_data_to_db():
+def write_sensors_data_to_db():
     for sensor in W1ThermSensor.get_available_sensors([W1ThermSensor.THERM_SENSOR_DS18B20]):
         print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
         redis_db.set('DS18B20_' + sensor.id, sensor.get_temperature())
@@ -26,12 +26,12 @@ sensor_status=redis_db.get('DS18B20_sensor_in_use')
 
 if sensor_status is None:
     sensor_lock(1)
-    wrtite_sensors_data_to_db()
+    write_sensors_data_to_db()
     sensor_lock(0)
 
 elif str(sensor_status) is '0' :
     sensor_lock(1)
-    wrtite_sensors_data_to_db()
+    write_sensors_data_to_db()
     sensor_lock(0)
 
 else:
