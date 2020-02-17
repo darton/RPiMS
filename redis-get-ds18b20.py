b2c8309b23b72fc740afe7406da7d8a9d74d6925
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-02-17 10:31:45 +0100

    Create redis-get-ds18b20.py

---
+++ redis-get-ds18b20.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import redis
+
+redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
+
+for sensor in redis_db.keys(pattern='DS18B20-*'):
+    print ( sensor + ' = {0:0.1f}*C '.format(float(redis_db.get(sensor))))
+
