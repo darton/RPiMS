9001889e00678fd8ea03bd8ce64aeb94091c210c
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-02-19 22:07:54 +0100

    Update CPUtemp.py

--- CPUtemp.py
+++ CPUtemp.py
@@ -5,8 +5,14 @@
 import redis
 from gpiozero import CPUTemperature
 
-cpu = CPUTemperature()
-print('CPU temperature: {0:0.0f}C'.format(cpu.temperature))
+def sensor_lock(lock_status):
+    redis_db.set('BME280_sensor_in_use', str(lock_status))
+
+
+def wrtite_sensor_data_to_db():
+    cpu = CPUTemperature()
+    redis_db.set('Temperature', data.temperature)  
+    print('CPU temperature: {0:0.0f}C'.format(cpu.temperature))
 
 redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
 redis_db.set('CPUtemperature', cpu.temperature)
