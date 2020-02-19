4bc1b9d274f55065acc3a98d0502b53cd4006efd
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-02-19 15:03:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ sudo apt-get dist-upgrade
 
 sudo apt-get -y install git-core python3-gpiozero python3-pip build-essential python3-dev python3-numpy python3-picamera
 sudo apt-get -y install libfreetype6-dev libopenjp2-7 libtiff5 libjpeg-dev
-sudo apt-get install python3-w1thermsensor
+sudo apt-get -y install python3-w1thermsensor
 
 sudo python3 -m pip install --upgrade pip setuptools wheel
 sudo pip3 install Adafruit_DHT RPi.bme280 redis pid
@@ -28,17 +28,16 @@ sudo apt-get -y install redis-server
 sudo systemctl enable redis-server.service
 sudo systemctl start redis-server.service
 
-sudo apt install nginx php php-fpm php-redis
+sudo apt -y install nginx php php-fpm php-redis
 echo "cgi.fix_pathinfo=0" |sudo tee -a /etc/php/7.0/fpm/php.ini
 sudo rm /var/www/html/index.html
 #sudo a2enmod proxy_fcgi setenvif
 #sudo a2enconf php7.0-fpm
-sudo systemctl restart php7.0-fpm
-sudo systemctl enable php7.0-fpm
+sudo systemctl restart php7.3-fpm
+sudo systemctl enable php7.3-fpm
 sudo mv $installdir/index.php /var/www/html/
-curl -sS https://raw.githubusercontent.com/darton/RPiMS/master/nginx.default > $installdir/nginx.default
 sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.org
-sudo mv $installdir/nginx.default /etc/nginx/sites-available/default
+curl -sS https://raw.githubusercontent.com/darton/RPiMS/master/nginx.default > /etc/nginx/sites-available/default
 sudo systemctl restart nginx
 sudo systemctl enable nginx
 
