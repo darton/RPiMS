406d9fd5fd160f0fa1ef4ac8a2c2a28c78be4dbf
darton <dariusz@kowalczyk.it>
2020-02-25 11:10:01 +0100

    show data in one line with ; as delimiter

--- redis-get-ds18b20.py
+++ redis-get-ds18b20.py
@@ -5,5 +5,4 @@ import redis
 redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
 
 for sensor in redis_db.keys(pattern='DS18B20-*'):
-    print ( sensor + ' = {0:0.1f}\xb0C '.format(float(redis_db.get(sensor))))
-
+    print(sensor + '={0:0.2f}'.format(float(redis_db.get(sensor))), end=';')
