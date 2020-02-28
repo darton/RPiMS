63cc81d3c2c7fed615e4c80bbb933bf3e1523dd0
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-02-28 11:09:57 +0100

    Update redis-get-data.py

--- redis-get-data.py
+++ redis-get-data.py
@@ -9,17 +9,20 @@ redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8",
 
 if len(sys.argv) > 1:
     if sys.argv[1] == 'BME280':
-        temperature = redis_db.get('BME280_Temperature')
-        humidity = redis_db.get('BME280_Humidity')
-        pressure = redis_db.get('BME280_Pressure')
-        print('Temperature={0:0.2f};Humidity={1:0.2f};Pressure={2:0.2f};'.format(float(temperature),float(humidity),float(pressure)))
+        if redis_db.get('use_BME280_sensor') == '1':
+            temperature = redis_db.get('BME280_Temperature')
+            humidity = redis_db.get('BME280_Humidity')
+            pressure = redis_db.get('BME280_Pressure')
+            print('Temperature={0:0.2f};Humidity={1:0.2f};Pressure={2:0.2f};'.format(float(temperature),float(humidity),float(pressure)))
     elif sys.argv[1] == 'DS18B20':
-        for sensor in redis_db.keys(pattern='DS18B20-*'):
-            print(sensor + '={0:0.2f}'.format(float(redis_db.get(sensor))), end=';')
-        print('')
+        if redis_db.get('use_DS18B20_sensor') == '1':
+            for sensor in redis_db.keys(pattern='DS18B20-*'):
+                print(sensor + '={0:0.2f}'.format(float(redis_db.get(sensor))), end=';')
+            print('')
     elif sys.argv[1] == 'DHT22':
-        temperature = redis_db.get('DHT22_Temperature')
-        humidity = redis_db.get('DHT22_Humidity')
-        print('Temperature={0:0.2f};Humidity={1:0.2f};'.format(float(temperature),float(humidity)))
+        if redis_db.get('use_DHT22_sensor') == '1':
+            temperature = redis_db.get('DHT22_Temperature')
+            humidity = redis_db.get('DHT22_Humidity')
+            print('Temperature={0:0.2f};Humidity={1:0.2f};'.format(float(temperature),float(humidity)))
 else:
     print('You must use one parameter from list BME280, DS18B20, DHT22')
