020286a5051477972c6a8886f13da8ea2ed267bc
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-03-02 16:04:16 +0100

    Update BME280.py

--- BME280.py
+++ BME280.py
@@ -2,40 +2,28 @@
 
 # -*- coding:utf-8 -*-
 
-import smbus2
-import bme280
-import redis
+from w1thermsensor import W1ThermSensor
 from time import sleep
-
-port = 1
-address = 0x77
-bus = smbus2.SMBus(port)
-
+import redis
 
 def sensor_lock(lock_status):
-    redis_db.set('BME280_sensor_in_use', str(lock_status))
+    redis_db.set('DS18B20_sensor_in_use', str(lock_status))
 
-def wrtite_sensor_data_to_db():
+def write_sensors_data_to_db():
     sensor_lock(1)
-    calibration_params = bme280.load_calibration_params(bus, address)
-    data = bme280.sample(bus, address, calibration_params)
-
-    redis_db.set('BME280_Humidity', data.humidity)
-    redis_db.set('BME280_Temperature', data.temperature)
-    redis_db.set('BME280_Pressure', data.pressure)
-
-    print('Humidity: {0:0.0f}%'.format(data.humidity))
-    print('Temperature: {0:0.1f}\xb0C'.format(data.temperature))
-    print('Pressure: {0:0.0f}hPa'.format(data.pressure))
-
-    sleep(1)
+    sensorslist = W1ThermSensor.get_available_sensors([W1ThermSensor.THERM_SENSOR_DS18B20])
+    for sensor in sensorslist:
+        sleep(0.5)
+        print("Sensor %s temperature %.2f"%(sensor.id,sensor.get_temperature()),"\xb0C")
+        redis_db.set('DS18B20-' + sensor.id, sensor.get_temperature())
     sensor_lock(0)
 
-
-redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
-sensor_status=redis_db.get('BME280_sensor_in_use')
-if str(sensor_status) is '0' or sensor_status is None:
-     wrtite_sensor_data_to_db()
-else:
-    print('The sensor is in use, please try again later')
-
+try:
+    redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
+    sensor_status=redis_db.get('DS18B20_sensor_in_use')
+    if str(sensor_status) is '0' or sensor_status is None:
+        write_sensors_data_to_db()
+    else:
+        print('The sensor is in use, please try again later')
+except (KeyboardInterrupt, SystemExit):
+    sensor_lock(0)
