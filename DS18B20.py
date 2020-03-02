162f1cf5951e64a5209cbae7e1599c3147ec5839
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-03-02 16:04:47 +0100

    Update DS18B20.py

--- DS18B20.py
+++ DS18B20.py
@@ -18,10 +18,12 @@ def write_sensors_data_to_db():
         redis_db.set('DS18B20-' + sensor.id, sensor.get_temperature())
     sensor_lock(0)
 
-
-redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
-sensor_status=redis_db.get('DS18B20_sensor_in_use')
-if str(sensor_status) is '0' or sensor_status is None:
-    write_sensors_data_to_db()
-else:
-    print('The sensor is in use, please try again later')
+try:
+    redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
+    sensor_status=redis_db.get('DS18B20_sensor_in_use')
+    if str(sensor_status) is '0' or sensor_status is None:
+        write_sensors_data_to_db()
+    else:
+        print('The sensor is in use, please try again later')
+except (KeyboardInterrupt, SystemExit):
+    sensor_lock(0)
