66e859c103fc60b85065b78d811ed430a28c95a3
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-03-26 13:19:49 +0100

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -11,23 +11,15 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 
-#raspivid -o - -t 0 -hf -w 640 -h 360 -fps 25 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554}' :demux=h264
 
-function stream_on { 
+function stream_on {
     raspivid_pid=$(pidof raspivid)
     vlc_pid=$(pidof vlc)
-    
-if [ $raspivid_pid -n ]; 
-then
-    pkill raspivid
-fi 
-   
-   if [ $vlc_pid -n]; 
-then
-    pkill vlc
-fi 
-    
-    raspivid -o - -t 0 -n -a 8 -a "Serwerownia_1 %Y-%m-%d %X" -fps 25 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554}' :demux=h264 &
+
+    if ([ ! -n "$raspivid_pid" ] && [ ! -n "$vlc_pid" ]) then
+        raspivid -o - -t 0 -n -a 8 -rot 270 -a "Serwerownia_1 %Y-%m-%d %X" -fps 30 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554}' :demux=h264 --h264-fps=30 &
+    fi
+
 }
 
 function stream_off {
