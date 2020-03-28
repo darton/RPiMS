c0603214830769c955d6e303ddfd4c99006ce78e
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-03-28 10:54:57 +0100

    Update sensors.py

--- sensors.py
+++ sensors.py
@@ -207,7 +207,17 @@ def motion_sensor_when_no_motion(ms_id):
     verbose = program_remote_control()
     if verbose is 'yes' :
         print("The " + str(ms_id) + ": no motion")
+    if use_picamera is 'yes':
+        door_sensor_values = []
+        motion_sensor_values = []
+        for s in door_sensor_list:
+            door_sensor_values.append(door_sensor_list[s].value)
+        for s in motion_sensor_list:
+            motion_sensor_values.append(int(not motion_sensor_list[s].value))
+        if all(door_sensor_values) and all(motion_sensor_values):
+            av_stream('stop')
 
+        
 def av_stream(state):
     if use_picamera is 'yes':
         subprocess.call("/home/pi/scripts/RPiMS/stream.sh" + " " +  state, shell=True)
