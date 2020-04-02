3babfbaa9985aea563f31c646e9d723a39e9b17a
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-04-02 22:42:21 +0200

    Update rpims.py

--- rpims.py
+++ rpims.py
@@ -36,7 +36,7 @@ config = {
     #Localization
     "location"               : "My Home",
     #verbose mode: yes/no
-    "verbose"                : yes,
+    "verbose"                : no,
     #use zabbix sender: yes/no
     "use_zabbix_sender"      : no,
     #use picamera: yes/no
@@ -49,8 +49,10 @@ config = {
     "use_motion_sensor"      : yes,
     #use LED indicator: yes/no
     "use_led_indicator"      : yes,
-    #use Waveshare display LCD/OLED HAT buttons and joystick
+    #use display HAT buttons and joystick
     "use_system_buttons"     : no,
+    #use i2c oled display
+    "use_i2c_oled"           : yes,
     #use CPU sensor: yes/no
     "use_CPU_sensor"         : yes,
     #use BME280 sensor: yes/no
@@ -264,6 +266,72 @@ def write_sensor_data(sensor_type):
             sleep(1)
 
 
+def oled_device():
+    from luma.core.interface.serial import i2c, spi, noop
+    from luma.core.render import canvas
+    from luma.core import lib
+    from luma.oled.device import sh1106
+    import RPi.GPIO as GPIO
+    import time
+    #import redis
+    import socket
+    from PIL import Image
+    from PIL import ImageDraw
+    from PIL import ImageFont
+    # Load default font.
+    font = ImageFont.load_default()
+    GPIO.setwarnings(False)
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    width = 128
+    height = 64
+    image = Image.new('1', (width, height))
+    # First define some constants to allow easy resizing of shapes.
+    padding = 0
+    top = padding
+    bottom = height-padding
+    # Move left to right keeping track of the current x position for drawing shapes.
+    x = 0
+    RST = 25
+    CS = 8
+    DC = 24
+    USER_I2C = 1
+    if  USER_I2C == 1:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(RST,GPIO.OUT)
+        GPIO.output(RST,GPIO.HIGH)
+
+        serial = i2c(port=1, address=0x3c)
+    else:
+        serial = spi(device=0, port=0, bus_speed_hz = 8000000, transfer_size = 4096, gpio_DC = 24, gpio_RST = 25)
+
+    device = sh1106(serial, rotate=2) #sh1106
+
+    try:
+        redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
+        while True:
+            with canvas(device) as draw:
+                hostname = socket.gethostname()
+                hostip = socket.gethostbyname(hostname)
+                #get data from redis db
+                temperature = round(float(redis_db.get('BME280_Temperature')),1)
+                humidity = round(float(redis_db.get('BME280_Humidity')),1)
+                pressure = round(float(redis_db.get('BME280_Pressure')),1)
+                door_sensor_1 = redis_db.get('door_sensor_1')
+                door_sensor_2 = redis_db.get('door_sensor_2')
+                cputemp = redis_db.get('CPU_Temperature')
+                #draw on oled
+                draw.text((x, top),       'IP:' + str(hostip), font=font, fill=255)
+                draw.text((x, top+9),     'Temperature..' + str(temperature) + '*C', font=font, fill=255)
+                draw.text((x, top+18),    'Humidity.....' + str(humidity) + '%',  font=font, fill=255)
+                draw.text((x, top+27),    'Pressure.....' + str(pressure) + 'hPa',  font=font, fill=255)
+                draw.text((x, top+36),    'Door 1.......' + str(door_sensor_1),  font=font, fill=255)
+                draw.text((x, top+45),    'Door 2.......' + str(door_sensor_2),  font=font, fill=255)
+                draw.text((x, top+54),    'CpuTemp......' + str(cputemp) + '*C', font=font, fill=255)
+    except:
+        print("The End")
+
+
 def threading_function(device_type):
     if device_type is 'BME280' :
         t1 = threading.Thread(target=write_sensor_data, args=("BME280",))
@@ -277,6 +345,10 @@ def threading_function(device_type):
         t3 = threading.Thread(target=write_sensor_data, args=("CPUtemp",))
         t3.daemon = True
         t3.start()
+    if device_type is 'oled' and config['use_i2c_oled'] is yes :
+        t4 = threading.Thread(target=oled_device)
+        t4.daemon = True
+        t4.start()
 
 # --- Main program ---
 
@@ -315,5 +387,6 @@ if config['use_system_buttons'] is yes :
 threading_function("CPUtemp")
 threading_function("BME280")
 threading_function("DS18B20")
+threading_function("oled")
 
 pause()
