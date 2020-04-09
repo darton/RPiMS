9e4802867b38fce77e6af6ac267f32a04b14485b
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-04-09 22:35:07 +0200

    change dir name from video to Video, add -"fps 30" parameter to MP4Box

--- videorecorder.sh
+++ videorecorder.sh
@@ -13,7 +13,7 @@
 
 now=$(date +%H%M%S_%m_%d_%Y)
 
-video_dir=/home/pi/video
+video_dir=/home/pi/Video
 video_file=pivideo_$now
 
 [[ -d $video_dir ]] || mkdir $video_dir
@@ -26,11 +26,12 @@ then
 fi 
 
 #raspivid -t 5000 -fps 25 -b 6000000  -a 8 -a "Serwerownia_1 %Y-%m-%d %X" -p 0,0,1920,1080 -o $video_dir/$video_file.h264
-raspivid -t 5000 -fps 25 -b 6000000  -a 8 -a "Serwerownia_1 %Y-%m-%d %X" -n -o $video_dir/$video_file.h264
+#raspivid  --profile high --level 4.2 -t 5000 -fps 30 -b 10000000  -a 8 -a "Serwerownia_1 %Y-%m-%d %X" -n -o $video_dir/$video_file.h264
+raspivid -t 5000 -fps 30 -b 6000000  -a 8 -a "Serwerownia_1 %Y-%m-%d %X" -n -o $video_dir/$video_file.h264
 
 cd $video_dir
 
-MP4Box -add $video_file.h264 $video_file.mp4
+MP4Box -fps 30 -add $video_file.h264 $video_file.mp4
 
 rm *.h264
 
