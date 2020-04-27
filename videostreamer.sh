e2e784a64aada048b84b997eeb79d8cf9c73a9b0
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-04-27 11:47:27 +0200

    Update videostreamer.sh

--- videostreamer.sh
+++ videostreamer.sh
@@ -11,13 +11,14 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 
+location=$(redis-cli get location)
 
 function stream_on {
     raspivid_pid=$(pidof raspivid)
     vlc_pid=$(pidof vlc)
 
     if ([ ! -n "$raspivid_pid" ] && [ ! -n "$vlc_pid" ]) then
-        raspivid -o - -t 0 -n -a 8 -rot 270 -a "Serwerownia_1 %Y-%m-%d %X" -fps 30 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554}' :demux=h264 --h264-fps=30 &
+        raspivid -o - -t 0 -n -a 8 -rot 270 -a "$location %Y-%m-%d %X" -fps 30 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554}' :demux=h264 --h264-fps=30 &
     fi
 
 }
