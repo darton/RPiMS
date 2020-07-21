cf65d178e98a64252583b5523ad191be05dc147d
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-07-21 13:36:53 +0200

    Update redis-get-data.py

--- redis-get-data.py
+++ redis-get-data.py
@@ -20,18 +20,18 @@ redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8",
 
 if len(sys.argv) > 1:
     if sys.argv[1] == 'BME280':
-        if redis_db.get('use_BME280_sensor') == '1':
+        if redis_db.get('use_BME280_sensor') == 'True':
             temperature = redis_db.get('BME280_Temperature')
             humidity = redis_db.get('BME280_Humidity')
             pressure = redis_db.get('BME280_Pressure')
             print('Temperature={0:0.2f};Humidity={1:0.2f};Pressure={2:0.2f};'.format(float(temperature),float(humidity),float(pressure)))
     elif sys.argv[1] == 'DS18B20':
-        if redis_db.get('use_DS18B20_sensor') == '1':
+        if redis_db.get('use_DS18B20_sensor') == 'True':
             for sensor in redis_db.keys(pattern='DS18B20-*'):
                 print(sensor + '={0:0.2f}'.format(float(redis_db.get(sensor))), end=';')
             print('')
     elif sys.argv[1] == 'DHT':
-        if redis_db.get('use_DHT_sensor') == '1':
+        if redis_db.get('use_DHT_sensor') == 'True':
             temperature = redis_db.get('DHT_Temperature')
             humidity = redis_db.get('DHT_Humidity')
             print('Temperature={0:0.2f};Humidity={1:0.2f};'.format(float(temperature),float(humidity)))
