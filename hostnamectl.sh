a5939f315b7860365f811ac4dffb47f8505e36b2
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-07-21 11:19:57 +0200

    Update hostnamectl.sh

--- hostnamectl.sh
+++ hostnamectl.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+_IP=$(hostname -I | awk '{print $1}') || true
+if [ "$_IP" ]; then
+  /usr/bin/redis-cli set hostip $_IP
+fi
+
+_hostname=$(/usr/bin/redis-cli get hostname)
+hostnamectl set-hostname $_hostname
+
+_location=$(/usr/bin/redis-cli get localtion)
+hostnamectl set-location "$_location"
+
+_chassis=$(/usr/bin/redis-cli get chassis)
+hostnamectl set-chassis $_chassis
+
+_deployment=$(/usr/bin/redis-cli get deployment)
+hostnamectl set-deployment $_deployment
+
+exit 0
