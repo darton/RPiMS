415208066f7f83473cf4678595244844b945b196
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-07-22 11:39:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ sudo apt-get -y install python3-w1thermsensor
 sudo apt-get -y install vlc ffmpeg gpac fbi
 
 sudo python3 -m pip install --upgrade pip setuptools wheel
-sudo -H pip3 install RPi.bme280 redis pid PyYAML
+sudo -H pip3 install --upgrade RPi.bme280 redis pid PyYAML
 sudo -H pip3 install --upgrade luma.oled
 sudo -H pip3 install --upgrade luma.lcd
 
