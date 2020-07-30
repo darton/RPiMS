61cd48a1fc14a87dc6139ae394aefef408b6e78b
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-07-30 11:35:55 +0200

    Create setup.php

---
+++ test/setup.php
@@ -0,0 +1,14 @@
+<table>
+<?php
+    foreach ($_POST as $key => $value) {
+        echo "<tr>";
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+        echo "<td>";
+        echo $value;
+        echo "</td>";
+        echo "</tr>";
+    }
+?>
+</table>
