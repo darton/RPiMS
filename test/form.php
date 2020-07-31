72f955c317bd318a230c9065a673c265bcefb9ba
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-07-31 16:35:10 +0200

    Update form.php

--- test/form.php
+++ test/form.php
@@ -1,19 +1,47 @@
-table>
+<table>
 <?php
 $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
+
     foreach ($_POST as $key => $value) {
         echo "<tr>";
         echo "<td>";
-        echo $key;
+        //echo $key;
         echo "</td>";
         echo "<td>";
-        echo $value;
+        //echo $value;
         echo "</td>";
         echo "</tr>";
-        $redis->set($key,$value);
+        //$redis->set($key,$value);
     }
 //$yaml = yaml_emit($_POST);
 //var_dump($yaml);
+
+$data = yaml_parse_file ("/home/pi/scripts/RPiMS/rpims.yaml");
+//print_r(array_keys($data));
+echo "<br/>";
+print_r(array_keys($data['setup']));
+echo "<br/>";
+echo "<br/>";
+print_r(array_keys($data['door_sensors']));
+echo "<br/>";
+echo "<br/>";
+print_r(array_keys($data['motion_sensors']));
+echo "<br/>";
+echo "<br/>";
+print_r(array_keys($data['system_buttons']));
+echo "<br/>";
+echo "<br/>";
+print_r(array_keys($data['led_indicators']));
+echo "<br/>";
+echo "<br/>";
+print_r(array_keys($data['zabbix_agent']));
+//print_r($_POST);
+
+
+yaml_emit_file ("/var/www/html/rpims/rpims2.yaml", $yaml);
+
 ?>
+
+
 </table>
