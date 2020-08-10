645e6c995457970bf37a63b56adb573d137c4af0
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-08-10 14:41:07 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,5 +1,11 @@
 <?php
 
+if ($_POST['GPIO_16']['type'] == 'ShutdownButton') {
+    $use_system_buttons = true;
+} else {
+    $use_system_buttons = false;
+}
+
 $setup = array(
     "verbose" => filter_var($_POST['verbose'], FILTER_VALIDATE_BOOLEAN),
     "use_zabbix_sender" => filter_var($_POST['use_zabbix_sender'], FILTER_VALIDATE_BOOLEAN),
@@ -7,7 +13,7 @@ $setup = array(
     "use_picamera_recording" => filter_var($_POST['use_picamera_recording'], FILTER_VALIDATE_BOOLEAN),
     "use_door_sensor" => filter_var($_POST['use_door_sensor'], FILTER_VALIDATE_BOOLEAN),
     "use_motion_sensor" => filter_var($_POST['use_motion_sensor'], FILTER_VALIDATE_BOOLEAN),
-    "use_system_buttons" => filter_var($_POST['use_system_buttons'], FILTER_VALIDATE_BOOLEAN),
+    "use_system_buttons" => filter_var($use_system_buttons, FILTER_VALIDATE_BOOLEAN),
     "use_led_indicators" => filter_var($_POST['use_led_indicators'], FILTER_VALIDATE_BOOLEAN),
     "use_serial_display" => filter_var($_POST['use_serial_display'], FILTER_VALIDATE_BOOLEAN),
     "use_serial_display" => filter_var($_POST['use_serial_display'], FILTER_VALIDATE_BOOLEAN),
@@ -36,8 +42,23 @@ $system_buttons = array();
 $motion_sensors = array();
 $led_indicators = array();
 
+$GPIO = array(
+    "GPIO_5" => $_POST['GPIO_5'],
+    "GPIO_6" => $_POST['GPIO_6'],
+    "GPIO_12" => $_POST['GPIO_12'],
+    "GPIO_13" => $_POST['GPIO_13'],
+    "GPIO_16" => $_POST['GPIO_16'],
+    "GPIO_17" => $_POST['GPIO_17'],
+    "GPIO_18" => $_POST['GPIO_18'],
+    "GPIO_19" => $_POST['GPIO_19'],
+    "GPIO_20" => $_POST['GPIO_20'],
+    "GPIO_21" => $_POST['GPIO_21'],
+    "GPIO_26" => $_POST['GPIO_26'],
+);
+
 $count = 1;
-foreach ($_POST as $key => $value) {
+
+foreach ($GPIO as $key => $value) {
     if ($value['type'] == 'DoorSensor'){
         $varname = 'door_sensor_'.$count;
         $door_sensors[$varname]['gpio_pin'] = (int)$value['gpio_pin'];
@@ -47,7 +68,7 @@ foreach ($_POST as $key => $value) {
 }
 
 $count = 1;
-foreach ($_POST as $key => $value) {
+foreach ($GPIO as $key => $value) {
     if ($value['type'] == 'MotionSensor'){
         $varname = 'motion_sensor_'.$count;
         $motion_sensors[$varname]['gpio_pin'] = (int)$value['gpio_pin'];
@@ -56,7 +77,7 @@ foreach ($_POST as $key => $value) {
 }
 
 $arrayName = 'shutdown_button';
-foreach ($_POST as $key => $value) {
+foreach ($GPIO as $key => $value) {
     if ($value['type'] == 'ShutdownButton'){
         $system_buttons[$arrayName] = [];
         $system_buttons[$arrayName]['gpio_pin'] = (int)$value['gpio_pin'] ;
@@ -64,7 +85,7 @@ foreach ($_POST as $key => $value) {
     }
 }
 
-foreach ($_POST as $key => $value) {
+foreach ($GPIO as $key => $value) {
     if ($value['type'] == 'door_led'){
         $led_indicators['door_led']['gpio_pin'] = (int)$value['gpio_pin'];
 }
