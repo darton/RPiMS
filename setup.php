6182b3d960606bc00af29f82edc0946415a2df73
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-08-11 11:36:21 +0200

    Update setup.php

--- setup.php
+++ setup.php
@@ -4,6 +4,7 @@
 <head>
 <title>RPiMS configuration</title>
 <meta charset="utf-8"/>
+<link rel="stylesheet" href="w3.css">
 <style>
 #t01 {
   width: 100%;
@@ -84,105 +85,105 @@ foreach ($system_buttons_gpio as $key => $value) {
 //var_dump($GPIO['GPIO_16']['hold_time']);
 
 ?>
-
-<form action="/form.php" method="post">
+<div class="w3-responsive">
+<form action="/form.php" method="post" class="w3-container">
 <fieldset>
 <legend>System configuration</legend>
-<table id="t01">
+<table id="t01" class="w3-table">
 <tr>
 <td>
-<label>Verbose:
+<label>Verbose:</label>
 </td>
 <td>
-<input name="verbose" type="hidden" value="no"><input name="verbose" type="checkbox" <?php if ($verbose) echo 'checked="checked"'; ?> value="True"></label>
+<input name="verbose" type="hidden" value="no"><input name="verbose" type="checkbox" class="w3-check" <?php if ($verbose) echo 'checked="checked"'; ?> value="True">
 </td>
 </tr>
 <tr>
 <td>
-<label>Use zabbix sender:
+<label>Use zabbix sender:</label>
 </td>
 <td>
-<input name="use_zabbix_sender" type="hidden" value="False"><input name="use_zabbix_sender" type="checkbox" <?php if ($use_zabbix_sender == 'yes') echo 'checked="checked"'; ?> value="True"></label>
-</td>
-</tr>
-<tr>
-<td>
-<label>Use picamera:
-</td>
-<td>
-<input name="use_picamera" type="hidden" value="False"><input name="use_picamera" type="checkbox" <?php if ($use_picamera == 'yes') echo 'checked="checked"'; ?> value="True"></label>
+<input name="use_zabbix_sender" type="hidden" value="False"><input name="use_zabbix_sender" type="checkbox" class="w3-check" <?php if ($use_zabbix_sender == 'yes') echo 'checked="checked"'; ?> value="True">
 </td>
 </tr>
 <tr>
 <td>
-<label>Use picamera recording:
+<label>Use picamera:</label>
 </td>
 <td>
-<input name="use_picamera_recording" type="hidden" value="False"><input name="use_picamera_recording" type="checkbox" <?php if ($use_picamera_recording == 'yes') echo 'checked="checked"'; ?> value="True"></label>
-</td>
-</tr>
-<tr>
-<td>
-<label>Use door sensor:
-</td>
-<td>
-<input name="use_door_sensor" type="hidden" value="False"><input name="use_door_sensor" type="checkbox" <?php if ($use_door_sensor == 'yes') echo 'checked="checked"'; ?> value="True"></label>
+<input name="use_picamera" type="hidden" value="False"><input name="use_picamera" type="checkbox" class="w3-check" <?php if ($use_picamera == 'yes') echo 'checked="checked"'; ?> value="True">
 </td>
 </tr>
 <tr>
 <td>
-<label>Use motion sensor:
+<label>Use picamera recording:</label>
 </td>
 <td>
-<input name="use_motion_sensor" type="hidden" value="False"><input name="use_motion_sensor" type="checkbox" <?php if ($use_motion_sensor == 'yes') echo 'checked="checked"'; ?> value="True"></label>
-</td>
-</tr>
-<tr>
-<td>
-<label>Use led indicators:
-</td>
-<td>
-<input name="use_led_indicators" type="hidden" value="False"><input name="use_led_indicators" type="checkbox" <?php if ($use_led_indicators == 'yes') echo 'checked="checked"'; ?> value="True"></label>
+<input name="use_picamera_recording" type="hidden" value="False"><input name="use_picamera_recording" type="checkbox" class="w3-check"  <?php if ($use_picamera_recording == 'yes') echo 'checked="checked"'; ?> value="True">
 </td>
 </tr>
 <tr>
 <td>
-<label>Use CPU sensor:
+<label>Use door sensor:</label>
 </td>
 <td>
-<input name="use_CPU_sensor" type="hidden" value="False"><input name="use_CPU_sensor" type="checkbox" <?php if ($use_CPU_sensor == 'yes') echo 'checked="checked"'; ?> value="True"></label>
-</td>
-</tr>
-<tr>
-<td>
-<label>Use BME280 sensor:
-</td>
-<td>
-<input name="use_BME280_sensor" type="hidden" value="False"><input name="use_BME280_sensor" type="checkbox" <?php if ($use_BME280_sensor == 'yes') echo 'checked="checked"'; ?> value="True"></label>
+<input name="use_door_sensor" type="hidden" value="False"><input name="use_door_sensor" type="checkbox" class="w3-check" <?php if ($use_door_sensor == 'yes') echo 'checked="checked"'; ?> value="True">
 </td>
 </tr>
 <tr>
 <td>
-<label>Use DS18B20 sensor:
+<label>Use motion sensor:</label>
 </td>
 <td>
-<input name="use_DS18B20_sensor" type="hidden" value="False"><input name="use_DS18B20_sensor" type="checkbox" <?php if ($use_DS18B20_sensor == 'yes') echo 'checked="checked"'; ?> value="True"></label>
-</td>
-</tr>
-<tr>
-<td>
-<label>Use DHT sensor:
-</td>
-<td>
-<input name="use_DHT_sensor" type="hidden" value="False"><input name="use_DHT_sensor" type="checkbox" <?php if ($use_DHT_sensor) echo 'checked="checked"'; ?> value="True"></label>
+<input name="use_motion_sensor" type="hidden" value="False"><input name="use_motion_sensor" type="checkbox" class="w3-check" <?php if ($use_motion_sensor == 'yes') echo 'checked="checked"'; ?> value="True">
 </td>
 </tr>
 <tr>
 <td>
-<label>Use serial display:
+<label>Use led indicators:</label>
 </td>
 <td>
-<input name="use_serial_display" type="hidden" value="False"><input name="use_serial_display" type="checkbox" <?php if ($use_serial_display == 'yes') echo 'checked="checked"'; ?> value="True"></label>
+<input name="use_led_indicators" type="hidden" value="False"><input name="use_led_indicators" type="checkbox" class="w3-check" <?php if ($use_led_indicators == 'yes') echo 'checked="checked"'; ?> value="True">
+</td>
+</tr>
+<tr>
+<td>
+<label>Use CPU sensor:</label>
+</td>
+<td>
+<input name="use_CPU_sensor" type="hidden" value="False"><input name="use_CPU_sensor" type="checkbox" class="w3-check" <?php if ($use_CPU_sensor == 'yes') echo 'checked="checked"'; ?> value="True">
+</td>
+</tr>
+<tr>
+<td>
+<label>Use BME280 sensor:</label>
+</td>
+<td>
+<input name="use_BME280_sensor" type="hidden" value="False"><input name="use_BME280_sensor" type="checkbox" class="w3-check" <?php if ($use_BME280_sensor == 'yes') echo 'checked="checked"'; ?> value="True">
+</td>
+</tr>
+<tr>
+<td>
+<label>Use DS18B20 sensor:</label>
+</td>
+<td>
+<input name="use_DS18B20_sensor" type="hidden" value="False"><input name="use_DS18B20_sensor" type="checkbox" class="w3-check" <?php if ($use_DS18B20_sensor == 'yes') echo 'checked="checked"'; ?> value="True">
+</td>
+</tr>
+<tr>
+<td>
+<label>Use DHT sensor:</label>
+</td>
+<td>
+<input name="use_DHT_sensor" type="hidden" value="False"><input name="use_DHT_sensor" type="checkbox" class="w3-check" <?php if ($use_DHT_sensor) echo 'checked="checked"'; ?> value="True">
+</td>
+</tr>
+<tr>
+<td>
+<label>Use serial display:</label>
+</td>
+<td>
+<input name="use_serial_display" type="hidden" value="False"><input name="use_serial_display" type="checkbox" class="w3-check" <?php if ($use_serial_display == 'yes') echo 'checked="checked"'; ?> value="True">
 </td>
 </tr>
 <tr>
@@ -190,16 +191,16 @@ foreach ($system_buttons_gpio as $key => $value) {
 <label for="serial_display_type">Serial display type</label>
 </td>
 <td>
-<select id="serial_display_type" name="serial_display_type">
+<select id="serial_display_type" name="serial_display_type" class="w3-select">
 <option value="oled_sh1106"<?php if ($serial_display_type == 'oled_sh1106') echo 'selected="selected"'; ?> >oled_sh1106</option>
 <option value="lcd_st7735"<?php if ($serial_display_type == 'lcd_st7735') echo 'selected="selected"'; ?> >lcd_st7735</option>
 </select>
 </td>
 <td>
-<label>Serial display refresh rate:
+<label>Serial display refresh rate:</label>
 </td>
 <td>
-<input name="serial_display_refresh_rate" type="number" min="1" max="50" value=<?= is_null($serial_display_refresh_rate) ? 0: 10 ?> size="2"></label>
+<input name="serial_display_refresh_rate" type="number" min="1" max="50" size="2" value="<?= is_null($serial_display_refresh_rate) ? 0: 10 ?>" >
 </td>
 </tr>
 </table>
@@ -208,42 +209,44 @@ foreach ($system_buttons_gpio as $key => $value) {
 
 <fieldset>
 <legend>Sensor configuration</legend>
-<table id="t01">
+<table id="t01" class="w3-table">
 <tr><td>
-<label>CPUtemp read interval:
+<label>CPUtemp read interval:</label>
 </td>
 <td>
-<input name="CPUtemp_read_interval" type="number" min="1" max="3600" value=<?= is_null($CPUtemp_read_interval) ? 0: 1 ?> size="4"></label>
+<input name="CPUtemp_read_interval" type="number" min="1" max="3600" class="w3-input" size="4" value="<?= is_null($CPUtemp_read_interval) ? 0: 1 ?>" >
 </td></tr>
 <tr><td>
-<label>BME280 read interval:
+<label>BME280 read interval:</label>
 </td>
 <td>
-<input name="BME280_read_interval" type="number" min="1" max="3600" value=<?= is_null($BME280_read_interval) ? 0: 10 ?> size="4"></label>
+<input name="BME280_read_interval" type="number" min="1" max="3600" class="w3-input" size="4" value="<?= is_null($BME280_read_interval) ? 0: 10 ?>" >
 </td>
 <td>
 <label for="BME280_i2c_address">BME280_i2c_address:</label>
 </td>
 <td>
-<select id="BME280_i2c_address" name="BME280_i2c_address">
+<select id="BME280_i2c_address" name="BME280_i2c_address" class="w3-select">
 <option value = 118 <?php if ($BME280_i2c_address == '118') echo 'selected="selected"'; ?> >0x76</option>
 <option value = 119 <?php if ($BME280_i2c_address == '119') echo 'selected="selected"'; ?> >0x77</option>
-</select><br />
+</select>
 </td></tr>
 <tr><td>
-<label>DS18B20 read interval:
+<label>DS18B20 read interval:</label>
 </td>
 <td>
-<input name="DS18B20_read_interval" type="number" min="1" max="3600" value=<?= is_null($DS18B20_read_interval) ? 0: 60 ?> size="4"></label>
+<input name="DS18B20_read_interval" class="w3-input" type="number" min="1" max="3600" size="4" value="<?= is_null($DS18B20_read_interval) ? 0: 60 ?>" >
 </td></tr>
 <tr><td>
-<label>DHT read interval:
+<label>DHT read interval:</label>
 </td><td>
-<input name="DHT_read_interval" type="number" min="1" max="3600"  value=<?= is_null($DHT_read_interval) ? 0: 5 ?> size="4"></label>
+<input name="DHT_read_interval" class="w3-input" type="number" min="1" max="3600" size="4" value="<?= is_null($DHT_read_interval) ? 0: 5 ?>" >
 </td>
 <td>
 <label for="DHT_type">DHT type:</label>
-<select id="DHT_type" name="DHT_type">
+</td>
+<td>
+<select id="DHT_type" name="DHT_type" class="w3-select">
   <option value="DHT11" <?php if ($DHT_type == 'DHT11') echo 'selected="selected"'; ?> >DHT11</option>
   <option value="DHT22" <?php if ($DHT_type == 'DHT22') echo 'selected="selected"'; ?> >DHT22</option>
   <option value="AM2302" <?php if ($DHT_type == 'AM2302') echo 'selected="selected"'; ?> >AM2302</option>
@@ -251,10 +254,12 @@ foreach ($system_buttons_gpio as $key => $value) {
 </td>
 <td>
 <label for="DHT_pin">DHT pin:</label>
-<select id="DHT_pin" name="DHT_pin">
+</td>
+<td>
+<select id="DHT_pin" name="DHT_pin" class="w3-select">
   <option value = 17 <?php if ($DHT_pin == '17') echo 'selected="selected"'; ?> >17</option>
   <option value = 18 <?php if ($DHT_pin == '18') echo 'selected="selected"'; ?> >18</option>
-</select><br />
+</select>
 </td></tr>
 </table>
 </fieldset>
@@ -262,13 +267,12 @@ foreach ($system_buttons_gpio as $key => $value) {
 
 <fieldset>
 <legend>Input configuration</legend>
-<table id="t01">
-
+<table id="t01" class="w3-table">
 <tr><td>
 <label for="GPIO_5">GPIO 5 input type:</label>
 </td>
 <td>
-<select id="GPIO_5" name="GPIO_5[type]" style="width: 125px;">
+<select id="GPIO_5" name="GPIO_5[type]" style="width: 160px;" class="w3-select">
   <option <?php if ($GPIO['GPIO_5']['type'] == 'DoorSensor') echo 'selected="selected"'; ?> value="DoorSensor">Door Sensor</option>
   <option <?php if ($GPIO['GPIO_5']['type'] == 'MotionSensor') echo 'selected="selected"'; ?> value="MotionSensor">Motion Sensor</option>
 </select>
@@ -289,7 +293,7 @@ if ($GPIO['GPIO_5']['type'] == 'DoorSensor'){
 <label for="GPIO_6">GPIO 6 input type:</label>
 </td>
 <td>
-<select id="GPIO_6" name="GPIO_6[type]" style="width: 125px;">
+<select id="GPIO_6" name="GPIO_6[type]" style="width: 160px;" class="w3-select">
   <option <?php if ($GPIO['GPIO_6']['type'] == 'DoorSensor') echo 'selected="selected"'; ?> value="DoorSensor">Door Sensor</option>
   <option <?php if ($GPIO['GPIO_6']['type'] == 'MotionSensor') echo 'selected="selected"'; ?> value="MotionSensor">Motion Sensor</option>
 </select>
@@ -310,7 +314,7 @@ if ($GPIO['GPIO_6']['type'] == 'DoorSensor'){
 <label for="GPIO_13">GPIO 13 input type:</label>
 </td>
 <td>
-<select id="GPIO_13" name="GPIO_13[type]" style="width: 125px;">
+<select id="GPIO_13" name="GPIO_13[type]" style="width: 160px;" class="w3-select">
   <option <?php if ($GPIO['GPIO_13']['type'] == 'DoorSensor') echo 'selected="selected"'; ?> value="DoorSensor">Door Sensor</option>
   <option <?php if ($GPIO['GPIO_13']['type'] == 'MotionSensor') echo 'selected="selected"'; ?> value="MotionSensor">Motion Sensor</option>
 </select>
@@ -332,7 +336,7 @@ if ($GPIO['GPIO_13']['type'] == 'DoorSensor'){
 <label for="GPIO_16">GPIO 16 input type:</label>
 </td>
 <td>
-<select id="GPIO_16" name="GPIO_16[type]" style="width: 125px;">
+<select id="GPIO_16" name="GPIO_16[type]" style="width: 160px;" class="w3-select">
   <option <?php if ($GPIO['GPIO_16']['type'] == 'DoorSensor') echo 'selected="selected"'; ?> value="DoorSensor">Door Sensor</option>
   <option <?php if ($GPIO['GPIO_16']['type'] == 'MotionSensor') echo 'selected="selected"'; ?> value="MotionSensor">Motion Sensor</option>
   <option <?php if ($GPIO['GPIO_16']['type'] == 'ShutdownButton') echo 'selected="selected"'; ?> value="ShutdownButton">Shutdown Button</option>
@@ -354,7 +358,7 @@ if ($GPIO['GPIO_16']['type'] == 'DoorSensor' OR $GPIO['GPIO_16']['type'] == 'Shu
 <label for="GPIO_17">GPIO 17 input type:</label>
 </td>
 <td>
-<select id="GPIO_17" name="GPIO_17[type]" style="width: 125px;">
+<select id="GPIO_17" name="GPIO_17[type]" style="width: 160px;" class="w3-select">
   <option <?php if ($GPIO['GPIO_17']['type'] == 'DoorSensor') echo 'selected="selected"'; ?> value="DoorSensor">Door Sensor</option>
   <option <?php if ($GPIO['GPIO_17']['type'] == 'MotionSensor') echo 'selected="selected"'; ?> value="MotionSensor">Motion Sensor</option>
 </select>
@@ -375,7 +379,7 @@ if ($GPIO['GPIO_17']['type'] == 'DoorSensor'){
 <label for="GPIO_19">GPIO 19 input type:</label>
 </td>
 <td>
-<select id="GPIO_19" name="GPIO_19[type]" style="width: 125px;">
+<select id="GPIO_19" name="GPIO_19[type]" style="width: 160px;" class="w3-select">
   <option <?php if ($GPIO['GPIO_19']['type'] == 'DoorSensor') echo 'selected="selected"'; ?> value="DoorSensor">Door Sensor</option>
   <option <?php if ($GPIO['GPIO_19']['type'] == 'MotionSensor') echo 'selected="selected"'; ?> value="MotionSensor">Motion Sensor</option>
 </select>
@@ -396,7 +400,7 @@ if ($GPIO['GPIO_19']['type'] == 'DoorSensor'){
 <label for="GPIO_20">GPIO 20 input type:</label>
 </td>
 <td>
-<select id="GPIO_20" name="GPIO_20[type]" style="width: 125px;">
+<select id="GPIO_20" name="GPIO_20[type]" style="width: 160px;" class="w3-select">
   <option <?php if ($GPIO['GPIO_20']['type'] == 'DoorSensor') echo 'selected="selected"'; ?> value="DoorSensor">Door Sensor</option>
   <option <?php if ($GPIO['GPIO_20']['type'] == 'MotionSensor') echo 'selected="selected"'; ?> value="MotionSensor">Motion Sensor</option>
 </select>
@@ -417,7 +421,7 @@ if ($GPIO['GPIO_20']['type'] == 'DoorSensor'){
 <label for="GPIO_21">GPIO 21 input type:</label>
 </td>
 <td>
-<select id="GPIO_21" name="GPIO_21[type]" style="width: 125px;">
+<select id="GPIO_21" name="GPIO_21[type]" style="width: 160px;" class="w3-select">
   <option <?php if ($GPIO['GPIO_21']['type'] == 'DoorSensor') echo 'selected="selected"'; ?> value="DoorSensor">Door Sensor</option>
   <option <?php if ($GPIO['GPIO_21']['type'] == 'MotionSensor') echo 'selected="selected"'; ?> value="MotionSensor">Motion Sensor</option>
 </select>
@@ -438,7 +442,7 @@ if ($GPIO['GPIO_21']['type'] == 'DoorSensor'){
 <label for="GPIO_26">GPIO 26 input type:</label>
 </td>
 <td>
-<select id="GPIO_26" name="GPIO_26[type]" style="width: 125px;">
+<select id="GPIO_26" name="GPIO_26[type]" style="width: 160px;" class="w3-select">
   <option <?php if ($GPIO['GPIO_26']['type'] == 'DoorSensor') echo 'selected="selected"'; ?> value="DoorSensor">Door Sensor</option>
   <option <?php if ($GPIO['GPIO_26']['type'] == 'MotionSensor') echo 'selected="selected"'; ?> value="MotionSensor">Motion Sensor</option>
 </select>
@@ -461,12 +465,12 @@ if ($GPIO['GPIO_26']['type'] == 'DoorSensor'){
 
 <fieldset>
 <legend>Output configuration</legend>
-<table id="t01">
+<table id="t01" class="w3-table">
 <tr><td>
 <label for="GPIO_12">GPIO 12:</label>
 </td>
 <td>
-<select id="GPIO_12" name="GPIO_12[type]" style="width: 125px;">
+<select id="GPIO_12" name="GPIO_12[type]" class="w3-select" style="width: 160px;">
   <option value="motion_led">Motion Indicator</option>
   <option selected value="door_led">Door Indicator</option>
 </select>
@@ -478,7 +482,7 @@ if ($GPIO['GPIO_26']['type'] == 'DoorSensor'){
 <label for="GPIO_18">GPIO 18:</label>
 </td>
 <td>
-<select id="GPIO_18" name="GPIO_18[type]" style="width: 125px;">
+<select id="GPIO_18" name="GPIO_18[type]" class="w3-select" style="width: 160px;">
   <option selected value="motion_led">Motion Indicator</option>
   <option value="door_led">Door Indicator</option>
 </select>
@@ -491,38 +495,39 @@ if ($GPIO['GPIO_26']['type'] == 'DoorSensor'){
 
 <fieldset>
 <legend>Zabbix Agent configuration</legend>
-<table id="t01">
+<table id="t01" class="w3-table">
 
 <tr><td>
-<label>Zabbix server:
+<label>Zabbix server:</label>
 </td>
 <td>
-<input name="zabbix_server" type="text" size="30" placeholder="zabbix.example.com" value="<?=$zabbix_server?>" pattern="^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$"></label>
+<input name="zabbix_server" type="text" size="30" placeholder="zabbix.example.com" value="<?=$zabbix_server?>" pattern="^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$">
 </td></tr>
 <tr><td>
-<label>Zabbix server Active:
+<label>Zabbix server Active:</label>
 </td>
 <td>
-<input name="zabbix_server_active"  type="text" size="30" value="<?=$zabbix_server_active?>" placeholder="zabbix.example.com" pattern="^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$"></label>
-</td></tr>
-
-<tr><td>
-<label>RPiMS location:
-</td>
-<td>
-<input name="location" type="text" size="30" value="<?=$location?>" ></label>
+<input name="zabbix_server_active"  type="text" size="30" value="<?=$zabbix_server_active?>" placeholder="zabbix.example.com" pattern="^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$">
 </td></tr>
 
 <tr><td>
-<label>RPiMS hostname:
+<label>RPiMS location:</label>
 </td>
 <td>
-<input name="hostname" type="text" size="30" value="<?=$hostname?>" ></label>
+<input name="location" type="text" size="30" value="<?=$location?>" >
+</td></tr>
+
+<tr><td>
+<label>RPiMS hostname:</label>
+</td>
+<td>
+<input name="hostname" type="text" size="30" value="<?=$hostname?>" >
 </td></tr>
 </table>
 </fieldset>
 
 <input type="submit" value="Save">
 </form>
+</div>
 </body>
 </html>
