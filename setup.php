8a24050ea0dfe710d5fe5800920a58da98ffc000
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-08-14 11:42:57 +0200

    Update setup.php

--- setup.php
+++ setup.php
@@ -194,7 +194,8 @@ foreach ($system_buttons_gpio as $key => $value) {
 </td>
 <td>
 <select id="serial_display_type" name="serial_display_type" class="w3-select">
-<option value="oled_sh1106"<?php if ($serial_display_type == 'oled_sh1106') echo 'selected="selected"'; ?> >oled_sh1106</option>
+<option value="oled_sh1106_i2c"<?php if ($serial_display_type == 'oled_sh1106_i2c') echo 'selected="selected"'; ?> >oled_sh1106_i2c</option>
+<option value="oled_sh1106_spi"<?php if ($serial_display_type == 'oled_sh1106_spi') echo 'selected="selected"'; ?> >oled_sh1106_spi</option>
 <option value="lcd_st7735"<?php if ($serial_display_type == 'lcd_st7735') echo 'selected="selected"'; ?> >lcd_st7735</option>
 </select>
 </td>
