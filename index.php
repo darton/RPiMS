fce033c10d9dc0df097f65e95d7fd96757f0b35e
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-08-20 16:15:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
+$redis->setOption(Redis::OPT_SCAN, Redis::SCAN_RETRY);
 
 $rpimskeys = $redis->keys('*');
 
@@ -9,6 +10,13 @@ foreach ($rpimskeys as $key) {
     $value = $redis->get($key);
     $rpims[$key] = $value;
 }
+
+
+//$it = NULL;
+//$rpims_door_sensors = $redis->scan($it, 'door_sensor_*',100);
+//$it = NULL;
+//$rpims_motion_sensors = $redis->scan($it, 'motion_sensor_*', 100);
+
 
 $rpims_yaml = yaml_parse_file ("/var/www/html/rpims.yaml");
 $motion_sensors = $rpims_yaml['motion_sensors'];
