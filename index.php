103dee41aa143f8e20572cc77c5bac242049afcb
Dariusz Kowalczyk <dariusz@kowalczyk.it>
2020-09-08 16:31:22 +0200

    update

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
-$redis->setOption(Redis::OPT_SCAN, Redis::SCAN_RETRY);
 
 $rpimskeys = $redis->keys('*');
 
@@ -11,15 +10,8 @@ foreach ($rpimskeys as $key) {
     $rpims[$key] = $value;
 }
 
-
-//$it = NULL;
-//$rpims_door_sensors = $redis->scan($it, 'door_sensor_*',100);
-//$it = NULL;
-//$rpims_motion_sensors = $redis->scan($it, 'motion_sensor_*', 100);
-
-
-$rpims_yaml = yaml_parse_file ("/var/www/html/rpims.yaml");
+$rpims_yaml = yaml_parse_file ("/var/www/html/conf/rpims.yaml");
 $motion_sensors = $rpims_yaml['motion_sensors'];
 $door_sensors = $rpims_yaml['door_sensors'];
 
-include 'template.html';
+include 'index_html.php';
