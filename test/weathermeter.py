186f396ef530a66962e2bf1a9d9f484ff5746e5c
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-09-22 10:35:26 +0200

    Update weathermeter.py

--- test/weathermeter.py
+++ test/weathermeter.py
@@ -1,42 +1,45 @@
 #!/usr/bin/env python3
 
 import automationhat
+import redis
 from time import sleep, time
 
 sleep(0.1)
 start = time()
 
 direction_mapr = {
-"N": 5127,
-"NNE": 5041,
-"NE": 6421,
-"ENE": 6240,
-"E": 17786,
-"ESE": 9267,
-"SE": 11701,
-"SSE": 7067,
-"S": 8274,
-"SSW": 5411,
-"SW": 5589,
-"W": 4820,
-"WNW": 4776,
-"NW" :4944,
-"NNW": 4866
+"N": 5080,
+"NNE": 5188,
+"NE": 6417,
+"ENE": 6253,
+"E": 17419,
+"SE": 11613,
+"ESE": 9380,
+"SSE": 6968,
+"S": 8129,
+"SSW": 5419,
+"SW": 5542,
+"W": 4781,
+"NW": 4977,
+"NNW": 4877,
 }
 
-
-Uwe = 5.42
+Uwe = 0
 Uwy = 0
 R1 = 4690
 R2 = 0
 
+redis_db = redis.StrictRedis(host="localhost", port=6379, db=1, charset="utf-8", decode_responses=True)
+redis_db.flushdb()
+
 while True:
-    Uwy = automationhat.analog.one.read()
-    Uwe = automationhat.analog.two.read()
+    Uwy = round(automationhat.analog.one.read(),1)
+    Uwe = round(automationhat.analog.two.read(),1)
     if Uwe != Uwy:
         R2 = int (Uwe/(Uwe - Uwy) * R1)
-        print(R2,Uwe,Uwy)
+        #print(R2,Uwe,Uwy)
     for item in direction_mapr:
-        if (R2 <= direction_mapr.get(item) * 1.009) and (R2 >= direction_mapr.get(item) * 0.991):
+        if (R2 <= direction_mapr.get(item) * 1.005) and (R2 >= direction_mapr.get(item) * 0.995):
             print(item)
-    sleep(0.25)
+            redis_db.set('wind_direction', item)
+    sleep(0.1)
