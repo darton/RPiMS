2110ed44f9f7cea2db9faeae4d1f0f758a1e0b77
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-09-24 15:42:19 +0200

    Update windspeed.py

--- test/windspeed.py
+++ test/windspeed.py
@@ -6,35 +6,35 @@ from gpiozero import Button
 from signal import pause
 from time import sleep, time
 
-redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
 
-wind_speed_acquisition_time = 6
-wind_speed_agregation_time = 3600
-wind_speed_sensor_pulse = 0
-wind_speeds = []
-pulse = 0
+def anemometer_pulse_counter():
+    global anemometer_pulse
+    anemometer_pulse += 1
 
-def pulse_counter():
-    global pulse
-    pulse += 1
-
-def reset_pulse_counter():
-    global pulse
-    pulse = 0
+def reset_anemometer_pulse_counter():
+    global anemometer_pulse
+    anemometer_pulse = 0
 
 def calculate_speed(wind_speed_acquisition_time):
-    global pulse
-    rotations = pulse/2
+    global anemometer_pulse
+    rotations = anemometer_pulse/2
     wind_speed_km_per_hour = round(rotations * 2.4/wind_speed_acquisition_time,1)
     return wind_speed_km_per_hour
 
 wind_speed_sensor = Button(21)
-wind_speed_sensor.when_pressed = pulse_counter
+wind_speed_sensor.when_pressed = anemometer_pulse_counter
+redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
+
+anemometer_pulse = 0
+wind_speed_acquisition_time = 6
+wind_speed_agregation_time = 3600
+wind_speeds = []
+
 
 while True:
     start_time = time()
     while time() - start_time <= wind_speed_acquisition_time:
-        reset_pulse_counter()
+        reset_anemometer_pulse_counter()
         sleep(wind_speed_acquisition_time)
         wind_speed = calculate_speed(wind_speed_acquisition_time)
         if len(wind_speeds) == (wind_speed_agregation_time/wind_speed_acquisition_time + 1):
