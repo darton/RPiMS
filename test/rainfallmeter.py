d7141a75fcc25fce78e64c0acab57ea939ba5857
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-09-24 11:18:27 +0200

    Update rainfallmeter.py

--- test/rainfallmeter.py
+++ test/rainfallmeter.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python3
 
+import redis
+import math
 from gpiozero import Button
 from signal import pause
 from time import sleep, time
 
 bucket_counter = 0
-interval = 10
+rainfall_acquisition_time = 6
+rainfalls = []
 
-BUCKET_SIZE = 0.2794
+BUCKET_SIZE = 0.2794 #[mm]
 
 def bucket_tipped():
     global bucket_counter
     bucket_counter += 1
 
-def reset_rainfall():
+def reset_bucket_counter():
     global bucket_counter
     bucket_counter = 0
 
+def calculate_rainfall():
+    rainfall = round(bucket_counter * BUCKET_SIZE,0)
+    return rainfall
+
+
+redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
 rain_sensor = Button(21, hold_time=0.1)
 rain_sensor.when_pressed = bucket_tipped
 
 while True:
-    rainfall = bucket_counter * BUCKET_SIZE
-    print(rainfall,bucket_counter)
-    reset_rainfall()
-    sleep(interval)
+    start_time = time()
+    while time() - start_time <= rainfall_acquisition_time:
+        reset_bucket_counter()
+        sleep(rainfall_acquisition_time)
+        rainfall = calculate_rainfall()
+        if len(rainfalls) == (3600*24/rainfall_acquisition_time + 1):
+            rainfalls.clear()
+        print (len(rainfalls))
+        rainfalls.append(rainfall)
+        print(rainfalls)
+    daily_rainfall = round(math.fsum(rainfalls),1)
+    print("Daily rainfall: " + str(daily_rainfall) + " mm")
+    redis_db.set('daily_rainfall', daily_rainfall)
