187c2ad9765f135e09527c42663204b74ae0fb40
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-09-25 10:54:05 +0200

    Update winddirection.py

--- test/winddirection.py
+++ test/winddirection.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python3
 
 import automationhat
-from time import sleep
+from time import sleep, time
 import redis
+import math
 
-sleep(0.1)
+sleep(0.1) # Delay for automationhat
+
+
+def get_average(angles):
+    sin_sum = 0.0
+    cos_sum = 0.0
+
+    for angle in angles:
+        r = math.radians(angle)
+        sin_sum += math.sin(r)
+        cos_sum += math.cos(r)
+
+    flen = float(len(angles))
+    s = sin_sum / flen
+    c = cos_sum / flen
+    arc = math.degrees(math.atan(s / c))
+    average = 0.0
+
+    if s > 0 and c > 0:
+        average = arc
+    elif c < 0:
+        average = arc + 180
+    elif s < 0 and c > 0:
+        average = arc + 360
+
+    return 0.0 if average == 360 else average
+
 
 direction_mapr = {
 "N": 5080,
@@ -35,29 +62,33 @@ direction_mapa = {
 "S": 180,
 "SSW": 202.5,
 "SW": 225,
-"W": 280,
+"W": 270,
 "NW": 315,
 "NNW": 337.5
 }
-
 
 Uwe = 5.2
 Uwy = 0
 R1 = 4690
 R2 = 0
+wind_direction_acquisition_time = 6
 angles = []
 
 redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, charset="utf-8", decode_responses=True)
 
-
 while True:
-    Uwy = round(automationhat.analog.one.read(),1)
-    Uwe = round(automationhat.analog.two.read(),1)
-    if Uwe != Uwy:
-        R2 = int (R1/(1 - Uwy/Uwe))
-    for item in direction_mapr:
-        if (R2 <= direction_mapr.get(item) * 1.005) and (R2 >= direction_mapr.get(item) * 0.995):
-            angles.append(direction_mapa.get(item))
-            print(direction_mapa.get(item), item)
-            redis_db.set('wind_direction', item)
-    sleep(0.1)
+    start_time = time()
+    angles.clear()
+    while time() - start_time <= wind_direction_acquisition_time:
+        Uwy = round(automationhat.analog.one.read(),1)
+        Uwe = round(automationhat.analog.two.read(),1)
+        if Uwe != Uwy:
+            R2 = int (R1/(1 - Uwy/Uwe))
+            #print(R2,Uwe,Uwy)
+        for item in direction_mapr:
+            if (R2 <= direction_mapr.get(item) * 1.005) and (R2 >= direction_mapr.get(item) * 0.995):
+                angles.append(direction_mapa.get(item))
+                average_wind_direction = int(round(get_average(angles),0))
+                #print(direction_mapa.get(item), item)
+    print("Average direction of wind: " + str(average_wind_direction))
+    redis_db.mset({'average_wind_direction': average_wind_direction, 'wind_direction': item})
