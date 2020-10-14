b6e411514c979b04c07363f9457b4d5e9d26b79a
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-10-14 09:38:33 +0200

    Update rpims.py

--- rpims.py
+++ rpims.py
@@ -3,7 +3,7 @@
 # -*- coding:utf-8 -*-
 #
 #  Author : Dariusz Kowalczyk
-#
+#  https://github.com/darton/RPiMS
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License Version 2 as
 #  published by the Free Software Foundation.
