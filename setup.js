c3b9090dedb96105ee2f8deb3b739c6f19831cb0
Dariusz Kowalczyk <dariusz@kowalczyk.it>
2020-11-09 14:43:19 +0100

    use jQuery added in setup

--- setup.js
+++ setup.js
@@ -1,15 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
 
 var tlspskidentityid = "TLSPSKIdentity";
 var tlspskid = "TLSPSK";
 
 /* Function to generate combination of PSK */
 function generateP(lenght) {
-	var pass = '';
+        var pass = '';
         var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
                     'abcdefghijklmnopqrstuvwxyz0123456789';
         for (i = 1; i <= lenght; i++) {
-        	var char = Math.floor(Math.random()
+                var char = Math.floor(Math.random()
                             * str.length + 1);
                 pass += str.charAt(char)
             }
@@ -19,5 +18,38 @@ function generateP(lenght) {
         function gfg_Run(psk_len,id) {
             document.getElementById(id).value =  generateP(psk_len);
         }
+
+document.addEventListener("DOMContentLoaded", () => {
+
+/* When the page is loaded it shows the input Hold Time for the GPIO Button input type. 
+Hides the field for another GPIO input type value.  */
+
+$('.gpioinputs').each(function(){
+            var selectedGPIOtype = $(this).children("option:selected").val();
+            var z = $(this).attr('id') + "_" + 'DS'
+            if (selectedGPIOtype == 'DoorSensor') {
+                $("#" + z).show();
+            }
+            else {
+                $("#" + z).hide();
+            }
+        });
+
+
+
+/* Shows input Hold Time when GPIO input type is selected as Button, 
+and sets default hold time value to 1. 
+Hides this field when other GPIO input type is selected. */
+$('.gpioinputs').change(function(){
+            var selectedGPIOtype = $(this).children("option:selected").val();
+            var z = $(this).attr('id') + "_" + 'DS'
+            if (selectedGPIOtype == 'DoorSensor') {
+                $("#" + z).show();
+		$("#" + z + "_HT").val("1");
+            }
+            else {
+                $("#" + z).hide();
+            }
+        });
 
 });
