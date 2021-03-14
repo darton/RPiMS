c48731c30c3b90033891c6a1b5eedaf7dc0bfe77
Dariusz Kowalczyk <darton@users.noreply.github.com>
2021-03-14 19:58:25 +0100

    serialtest.py

---
+++ test/serialtest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import time
+import serial
+ser = serial.Serial(
+ port='/dev/ttyACM0',
+ baudrate = 38400,
+ parity=serial.PARITY_NONE,
+ stopbits=serial.STOPBITS_ONE,
+ bytesize=serial.EIGHTBITS,
+ timeout=2
+)
+
+counter = 0
+while counter < 10:
+    x=ser.readline()
+    print(x)
+    counter += 1
