0dcf8475bdaa99429caa41cc94b782bbf008c5f9
Dariusz Kowalczyk <darton@users.noreply.github.com>
2021-03-15 09:59:16 +0100

    Update main.py

--- test/main.py
+++ test/main.py
@@ -2,16 +2,15 @@ from machine import Pin, I2C
 from time import sleep
 import BME280
 
-sda=Pin(16)
-scl=Pin(17)
-
+sda = Pin(16)
+scl = Pin(17)
 i2c=machine.I2C(0,sda=sda, scl=scl, freq=400000)
+BME280_i2c_address = 0x77
+
 while True:
-    bme = BME280.BME280(i2c=i2c)
+    bme = BME280.BME280(address=BME280_i2c_address,i2c=i2c)
     temp = bme.temperature
     hum = bme.humidity
     pres = bme.pressure
     print(temp,hum,pres)
     sleep(1)
-
- 
