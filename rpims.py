3a72a624f9b012901b122f5a893b5012ca605057
Dariusz Kowalczyk <dariusz@kowalczyk.it>
2021-03-19 23:19:42 +0100

    improvements in function get_bme280_data

--- rpims.py
+++ rpims.py
@@ -194,14 +194,16 @@ def get_bme280_data(**kwargs):
             print(f'Problem with sensor BME280: {err}')
 
     if interface_type == 'serial':
-        def serial_data(port, baudrate):
+        def serial_data(port, baudrate, timeout):
             import serial
-            ser = serial.Serial(port, baudrate)
+            ser = serial.Serial(port, baudrate, timeout)
             while True:
                 yield ser.readline()
 
-        for line in serial_data('/dev/ttyACM0', 38400):
+        for line in serial_data('/dev/ttyACM0', 115200, 5):
             msg = line.decode('utf-8').split()
+            if len(msg)< 3:
+                continue
             redis_db.mset({'BME280_Temperature': msg[0], 'BME280_Humidity': msg[1], 'BME280_Pressure': msg[2]})
             redis_db.expire('BME280_Temperature', read_interval*2)
             redis_db.expire('BME280_Humidity', read_interval*2)
@@ -209,6 +211,7 @@ def get_bme280_data(**kwargs):
             if bool(verbose) is True:
                 print(f'BME280 on serial: Temperature: {msg[0]} °C, Humidity: {msg[1]} %, Pressure: {msg[2]} hPa')
             sleep(read_interval)
+
 
 def get_ds18b20_data(**kwargs):
     verbose = kwargs['verbose']
