2141aa2b60863be627c217011d42fba3a03adfaf
Dariusz Kowalczyk <darton@users.noreply.github.com>
2021-03-23 21:49:04 +0100

    Update videostreamer.sh

--- test/videostreamer.sh
+++ test/videostreamer.sh
@@ -28,13 +28,14 @@ function stream_on {
         if [ ! -e /var/www/html/streaming/hls.js ]; then
             cp /var/www/html/stream/hls.js /var/www/html/streaming/
         fi
-
-        #raspivid -w 640 -h 480  -o - -t 0 -n -a 8 -rot 0 -a "$location %Y-%m-%d %X" -fps 30 | cvlc --rtsp-timeout 10 -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554}' :demux=h264 --h264-fps=30 &
-        #raspivid -o - -t 0 -n -w 1280 -h 1024 -rot 0 -a "$location %Y-%m-%d %X" -fps 30 | cvlc  --rtsp-timeout 1000  -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264 --h264-fps=30 --sout-rtsp-user rpims --sout-rtsp-pwd password &
-        ##/usr/bin/ffmpeg -input_format h264 -f video4linux2 -video_size 1640x922 -framerate 30 -i /dev/video0 -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
-        #/usr/bin/ffmpeg -input_format h264 -f video4linux2 -video_size 640x480 -framerate 30 -i /dev/video0 -use_wallclock_as_timestamps 1 -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags independent_segments+delete_segments /dev/shm/streaming/live.m3u8
-        #/usr/bin/ffmpeg -input_format h264 -f video4linux2 -video_size 640x480 -framerate 30 -i /dev/video0 -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags split_by_time+delete_segments /dev/shm/streaming/live.m3u8
-        /usr/bin/ffmpeg -input_format h264 -f video4linux2 -video_size 640x480 -framerate 30 -i /dev/video0 -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
+            #raspivid -w 640 -h 480  -o - -t 0 -n -a 8 -rot 0 -a "$location %Y-%m-%d %X" -fps 30 | cvlc --rtsp-timeout 10 -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554}' :demux=h264 --h264-fps=30 &
+            #raspivid -o - -t 0 -n -w 1280 -h 1024 -rot 0 -a "$location %Y-%m-%d %X" -fps 30 | cvlc  --rtsp-timeout 1000  -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264 --h264-fps=30 --sout-rtsp-user rpims --sout-rtsp-pwd password &
+            ##/usr/bin/ffmpeg -input_format h264 -f video4linux2 -video_size 1640x922 -framerate 30 -i /dev/video0 -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
+            #/usr/bin/ffmpeg -input_format h264 -f video4linux2 -video_size 640x480 -framerate 30 -i /dev/video0 -use_wallclock_as_timestamps 1 -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags independent_segments+delete_segments /dev/shm/streaming/live.m3u8
+            #/usr/bin/ffmpeg -input_format h264 -f video4linux2 -video_size 640x480 -framerate 30 -i /dev/video0 -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags split_by_time+delete_segments /dev/shm/streaming/live.m3u8
+            #/usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -input_format h264 -f video4linux2 -video_size 640x480 -framerate 30 -i /dev/video0 -map_metadata 0 -metadata:s:v rotate="90" -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags delete_segments        #/usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -input_format h264 -f video4linux2  -video_size 640x480 -framerate 30 -i /dev/video0 -c:v copy -f hls -hls_time 6 -hls_list_size 6 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
+            #raspivid -n -o - -t 0 -vf -hf -fps 30 -b 6000000 -rot 270 -w 640 -h 480 | /usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -f h264 -i - -c:v copy -f hls -hls_time 6 -hls_list_size 3 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
+            raspivid -n -o - -t 0 -fps 25 -b 6000000 -rot 90 -w 640 -h 480 | /usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -f h264 -i - -c:v copy -f hls -hls_time 6 -hls_list_size 30 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
     else
         echo "ffmpeg is already running !"
     fi
