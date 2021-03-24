cef2c69401c2d11cb7ff17db74027b455abec9b0
Dariusz Kowalczyk <darton@users.noreply.github.com>
2021-03-24 11:06:01 +0100

    Update videostreamer.sh

--- test/videostreamer.sh
+++ test/videostreamer.sh
@@ -10,12 +10,14 @@
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-
+# https://cdn.jsdelivr.net/npm/hls.js@latest/dist/hls.min.js.map
+# https://cdn.jsdelivr.net/npm/hls.js@latest/dist/hls.min.js
 
 function stream_on {
     ffmpeg_pid=$(pidof ffmpeg)
+    raspivid_pid=$(pidof raspivid)
 
-    if ([ ! -n "$ffmpeg_pid" ]) then
+    if ([ ! -n "$ffmpeg_pid" ] && [ ! -n "$raspivid_pid" ]  ) then
 
         [[ -d /dev/shm/streaming ]] || mkdir -p /dev/shm/streaming
 
@@ -25,25 +27,37 @@ function stream_on {
         if [ ! -e /var/www/html/streaming/index.html ]; then
             cp /var/www/html/stream/index.html /var/www/html/streaming/
         fi
-        if [ ! -e /var/www/html/streaming/hls.js ]; then
-            cp /var/www/html/stream/hls.js /var/www/html/streaming/
+        if [ ! -e /var/www/html/streaming/hls.min.js ]; then
+            cp /var/www/html/stream/hls.min.js /var/www/html/streaming/
         fi
+        if [ ! -e /var/www/html/streaming/hls.min.js.map ]; then
+            cp /var/www/html/stream/hls.min.js.map /var/www/html/streaming/
+        fi
+
             #raspivid -w 640 -h 480  -o - -t 0 -n -a 8 -rot 0 -a "$location %Y-%m-%d %X" -fps 30 | cvlc --rtsp-timeout 10 -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554}' :demux=h264 --h264-fps=30 &
             #raspivid -o - -t 0 -n -w 1280 -h 1024 -rot 0 -a "$location %Y-%m-%d %X" -fps 30 | cvlc  --rtsp-timeout 1000  -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264 --h264-fps=30 --sout-rtsp-user rpims --sout-rtsp-pwd password &
             ##/usr/bin/ffmpeg -input_format h264 -f video4linux2 -video_size 1640x922 -framerate 30 -i /dev/video0 -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
             #/usr/bin/ffmpeg -input_format h264 -f video4linux2 -video_size 640x480 -framerate 30 -i /dev/video0 -use_wallclock_as_timestamps 1 -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags independent_segments+delete_segments /dev/shm/streaming/live.m3u8
             #/usr/bin/ffmpeg -input_format h264 -f video4linux2 -video_size 640x480 -framerate 30 -i /dev/video0 -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags split_by_time+delete_segments /dev/shm/streaming/live.m3u8
-            #/usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -input_format h264 -f video4linux2 -video_size 640x480 -framerate 30 -i /dev/video0 -map_metadata 0 -metadata:s:v rotate="90" -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags delete_segments        #/usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -input_format h264 -f video4linux2  -video_size 640x480 -framerate 30 -i /dev/video0 -c:v copy -f hls -hls_time 6 -hls_list_size 6 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
+            #/usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -input_format h264 -f video4linux2 -video_size 640x480 -framerate 30 -i /dev/video0 -map_metadata 0 -metadata:s:v rotate="90" -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
+            #/usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -input_format h264 -f video4linux2  -video_size 640x480 -framerate 30 -i /dev/video0 -c:v copy -f hls -hls_time 6 -hls_list_size 6 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
             #raspivid -n -o - -t 0 -vf -hf -fps 30 -b 6000000 -rot 270 -w 640 -h 480 | /usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -f h264 -i - -c:v copy -f hls -hls_time 6 -hls_list_size 3 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
-            raspivid -n -o - -t 0 -fps 25 -b 6000000 -rot 90 -w 640 -h 480 | /usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -f h264 -i - -c:v copy -f hls -hls_time 6 -hls_list_size 30 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
+
+            #/usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -input_format h264 -f video4linux2 -video_size 640x480 -framerate 30 -i /dev/video0 -c:v copy -f hls -hls_time 1 -hls_list_size 30 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
+
+            #/usr/bin/raspivid -n -o - -t 0 -rot 0 -w 640 -h 480 -fps 25 -b 6000000 | /usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -f h264 -i - -c:v copy -f hls -hls_time 6 -hls_list_size 30 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
+
+            /usr/bin/raspivid -n -o - -t 0 -rot 0 -w 640 -h 480 -fps 25 -b 3000000| /usr/bin/ffmpeg -y -hide_banner -use_wallclock_as_timestamps 1 -f h264 -i - -c:v copy -f hls -hls_time 2 -hls_list_size 5 -hls_flags delete_segments /dev/shm/streaming/live.m3u8
+
     else
-        echo "ffmpeg is already running !"
+            echo "ffmpeg or raspivid is already running !"
     fi
 
 }
 
 function stream_off {
     pkill ffmpeg
+    pkill raspivid
     exit 0
 }
 
