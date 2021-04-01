96231002f7aff21cf3984cff55ac20055054678b
Dariusz Kowalczyk <darton@users.noreply.github.com>
2021-04-01 16:53:48 +0200

    Update main.py

--- pico/main.py
+++ pico/main.py
@@ -4,13 +4,13 @@ import BME280
 
 sda = Pin(16)
 scl = Pin(17)
-i2c=machine.I2C(0,sda=sda, scl=scl, freq=400000)
+i2c=machine.I2C(0,sda=sda, scl=scl, freq=100000)
 BME280_i2c_address = 0x77
 
 while True:
     bme = BME280.BME280(address=BME280_i2c_address,i2c=i2c)
-    temp = bme.temperature
-    hum = bme.humidity
-    pres = bme.pressure
+    temp = int(float(bme.temperature)*1000)
+    hum = int(float(bme.humidity)*1000)
+    pres = int(float(bme.pressure)*1000)
     print(temp,hum,pres)
     sleep(1)
