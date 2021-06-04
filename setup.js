4f4e872c9fae1f293ca10f2e915f06fa70fa180c
Dariusz Kowalczyk <dariusz@kowalczyk.it>
2021-06-04 11:37:28 +0200

    visual improvements

--- setup.js
+++ setup.js
@@ -7,10 +7,10 @@ function generateP(lenght) {
         var pass = '';
         var str = 'abcdef0123456789';
         for (i = 1; i <= lenght; i++) {
-                var char = Math.floor(Math.random()
-                            * str.length + 1);
-                pass += str.charAt(char)
-            }
+            var char = Math.floor(Math.random()
+            * str.length + 1);
+            pass += str.charAt(char)
+        }
             return pass;
         }
 
@@ -22,8 +22,8 @@ function generateP(lenght) {
 function showFunction(id) {
             var x = document.getElementById(id);
             if (x.className.indexOf("w3-show") == -1) {
-              x.className = x.className.replace(" w3-hide", "");
-              x.className += " w3-show";
+                x.className = x.className.replace(" w3-hide", "");
+                x.className += " w3-show";
             } 
           }
         
@@ -38,10 +38,10 @@ function hideFunction(id) {
 function gpioSH(id) {
             var x = document.getElementById(id);
             if (x.className.indexOf("w3-show") == -1) {
-              x.className += " w3-show";
+                x.className += " w3-show";
             } 
             else {
-              x.className = x.className.replace(" w3-show", "");
+                x.className = x.className.replace(" w3-show", "");
             }
           }
 
@@ -53,15 +53,16 @@ var select3 = document.body.querySelector('#id3_BME280_serial_port_select')
 
 var select3si = document.body.querySelector('#id3_BME280_serial_port_select').selectedIndex
 for ( var i=0; i<select2.length; i++ ) {
-    if (select2.options[i].value == select3.options[select3si].value)
-	select2.remove(i);
+    if (select2.options[i].value == select3.options[select3si].value){
+	    select2.remove(i);
     }
+}
 
 var select2si = document.body.querySelector('#id2_BME280_serial_port_select').selectedIndex
 for ( var i=0; i<select3.length; i++ ) {
-    if (select3.options[i].value == select2.options[select2si].value)
-	select3.remove(i);
+    if (select3.options[i].value == select2.options[select2si].value) {
     }
+}
 
 var id2_selectedport = $('#id2_BME280_serial_port_select').children("option:selected").val();
 //var id2_selectedid = $('#id2_BME280_serial_port_select').attr('id');
@@ -72,17 +73,17 @@ $('.serial-port').change(function(){
     var selectedport = $(this).children("option:selected").val();
     var selectedid = $(this).attr('id');
     if (selectedid == 'id2_BME280_serial_port_select'){
-	$('#id3_BME280_serial_port_select').find('option[value='+ selectedport +']').remove();
-	$('#id3_BME280_serial_port_select').append($("<option></option>").attr("value", id2_selectedport).text(id2_selectedport));
-	id2_selectedport = $('#id2_BME280_serial_port_select').children("option:selected").val();
-	//console.log(selectedport,selectedid);
+        $('#id3_BME280_serial_port_select').find('option[value='+ selectedport +']').remove();
+        $('#id3_BME280_serial_port_select').append($("<option></option>").attr("value", id2_selectedport).text(id2_selectedport));
+        id2_selectedport = $('#id2_BME280_serial_port_select').children("option:selected").val();
+        //console.log(selectedport,selectedid);
     }
 
     if (selectedid == 'id3_BME280_serial_port_select'){
-	$('#id2_BME280_serial_port_select').find('option[value='+ selectedport +']').remove();
-	$('#id2_BME280_serial_port_select').append($("<option></option>").attr("value", id3_selectedport).text(id3_selectedport));
-	id3_selectedport = $('#id3_BME280_serial_port_select').children("option:selected").val();
-	//console.log(selectedport,selectedid);
+        $('#id2_BME280_serial_port_select').find('option[value='+ selectedport +']').remove();
+        $('#id2_BME280_serial_port_select').append($("<option></option>").attr("value", id3_selectedport).text(id3_selectedport));
+        id3_selectedport = $('#id3_BME280_serial_port_select').children("option:selected").val();
+        //console.log(selectedport,selectedid);
     }
 })
 
