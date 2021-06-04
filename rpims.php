f14b4507b42d7fe3bf2b05446af2245ad7fab0e2
Dariusz Kowalczyk <dariusz@kowalczyk.it>
2021-06-04 11:41:36 +0200

    visual improvements

--- rpims.php
+++ rpims.php
@@ -39,45 +39,41 @@ $rpims_api["system"]["hostname"] = $zabbix_agent["hostname"];
 $rpims_api["system"]["location"] = $zabbix_agent["location"];
 
 
-if ($config["use_CPU_sensor"] == true){
+if ($config["use_CPU_sensor"] == true) {
     $rpims_api["sensors"]["cpu"]["temperature"] = $rpims["CPU_Temperature"];
 }
 
-if ($config["use_BME280_sensor"] == true){
+if ($config["use_BME280_sensor"] == true) {
+    foreach ($sensors['BME280'] as $key => $value) {
+        $id = $sensors['BME280'][$key]["id"];
+        $t = $id."_BME280_Temperature";
+        $h = $id."_BME280_Humidity";
+        $p = $id."_BME280_Pressure";
 
-    foreach ($sensors['BME280'] as $key => $value)
-    {
-	$id = $sensors['BME280'][$key]["id"];
-	$t = $id."_BME280_Temperature";
-	$h = $id."_BME280_Humidity";
-	$p = $id."_BME280_Pressure";
-
-        if ($sensors["BME280"][$id]["use"] == true)
-	{
-	    $rpims_api["sensors"]["bme280"][$id]["name"] = $sensors["BME280"][$id]["name"] ;
-	    $rpims_api["sensors"]["bme280"][$id]["temperature"] = $rpims[$t] ;
-	    $rpims_api["sensors"]["bme280"][$id]["humidity"] = $rpims[$h] ;
-	    $rpims_api["sensors"]["bme280"][$id]["pressure"] = $rpims[$p] ;
-	}
+        if ($sensors["BME280"][$id]["use"] == true) {
+            $rpims_api["sensors"]["bme280"][$id]["name"] = $sensors["BME280"][$id]["name"] ;
+            $rpims_api["sensors"]["bme280"][$id]["temperature"] = $rpims[$t] ;
+            $rpims_api["sensors"]["bme280"][$id]["humidity"] = $rpims[$h] ;
+            $rpims_api["sensors"]["bme280"][$id]["pressure"] = $rpims[$p] ;
+	    }
     }
 }
 
-if ($config["use_DS18B20_sensor"] == true){
+if ($config["use_DS18B20_sensor"] == true) {
     $DS18B20_sensors = $redis->smembers('DS18B20_sensors');
-    foreach ($DS18B20_sensors as $key => $value){
-	$ds18b20_name = $value."_name";
+    foreach ($DS18B20_sensors as $key => $value) {
+        $ds18b20_name = $value."_name";
         $rpims_api["sensors"]["one_wire"]["ds18b20"]["$value"]["temperature"] = $rpims[$value];
         $rpims_api["sensors"]["one_wire"]["ds18b20"]["$value"]["name"] = $sensors["ONE_WIRE"]["DS18B20"]["addresses"]["$value"]["name"];
-        }
     }
+}
 
-if ($config["use_DHT_sensor"] == true){
-
+if ($config["use_DHT_sensor"] == true) {
     $rpims_api["sensors"]["dht"]["temperature"] = $rpims["DHT_Temperature"];
     $rpims_api["sensors"]["dht"]["humidity"] = $rpims["DHT_Humidity"];
 }
 
-if ($config["use_weather_station"] == true){
+if ($config["use_weather_station"] == true) {
     $rpims_api["weather_station"]["wind_speed"] = $rpims["wind_speed"];
     $rpims_api["weather_station"]["average_wind_speed"] = $rpims["average_wind_speed"];
     $rpims_api["weather_station"]["daily_average_wind_speed"] = $rpims["daily_average_wind_speed"];
@@ -90,25 +86,25 @@ if ($config["use_weather_station"] == true){
 $obj = $redis-> get('gpio');
 $gpio = json_decode($obj, true);
 
-if ($config["use_door_sensor"] == true){
+if ($config["use_door_sensor"] == true) {
     foreach ($gpio as $key=> $value) {
-	if ($gpio[$key]["type"] == "DoorSensor" ){
-	    $door_sensors[$key] = ($gpio[$key]);
-	}
+        if ($gpio[$key]["type"] == "DoorSensor" ) {
+            $door_sensors[$key] = ($gpio[$key]);
+        }
     }
     foreach ($door_sensors as $key => $value){
-	$rpims_api["sensors"]["door_sensors"]["$key"] = $rpims[$key];
+	    $rpims_api["sensors"]["door_sensors"]["$key"] = $rpims[$key];
     }
 }
 
 if ($config["use_motion_sensor"] == true){
     foreach ($gpio as $key=> $value) {
-	if ($gpio[$key]["type"] == "MotionSensor" ){
-	    $motion_sensors[$key] = ($gpio[$key]);
-	}
+        if ($gpio[$key]["type"] == "MotionSensor" ) {
+            $motion_sensors[$key] = ($gpio[$key]);
+        }
     }
-    foreach ($motion_sensors as $key => $value){
-	$rpims_api["sensors"]["motion_sensors"]["$key"] = $rpims[$key];
+    foreach ($motion_sensors as $key => $value) {
+	    $rpims_api["sensors"]["motion_sensors"]["$key"] = $rpims[$key];
     }
 }
 
