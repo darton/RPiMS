03f470e806066175919bf0a7f834bb858e8fc434
Dariusz Kowalczyk <darton@users.noreply.github.com>
2021-06-23 17:05:43 +0200

    Update index_html.php

--- index_html.php
+++ index_html.php
@@ -42,7 +42,7 @@
       <?php if ($config["use_picamera"] == "True") {
         echo '<div class="sensors">';
         echo '<div><a class="sensors" href="http://'.$server_ip.':8080/stream/video.mjpeg">Video Stream URL</a></div>';
-        echo '<div><iframe src="http://'.$server_ip.':8080/stream/video.mjpeg" width="640" height="480" style="border:none;" title="Video Stream"></iframe></
+        echo '<div><iframe src="http://'.$server_ip.':8080/stream/video.mjpeg" width="640" height="480" style="border:none;" title="Video Stream"></iframe>;
         echo '</div>';
        }
       ?>
