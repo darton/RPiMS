fb6b06a7d7fe3a98635327512b638af7c62c0de3
Dariusz Kowalczyk <dariusz@kowalczyk.it>
2021-11-18 10:50:11 +0100

    new version of upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -8,23 +8,39 @@ wwwdir=/var/www/html
 _IP=$(ip route get 1.1.1.1 | awk '{print $7}')
 
 echo "Do you want to upgrade the RPiMS ?"
-read -r -p "$1 [y/N] " response < /dev/tty
-if [[ $response =~ ^(yes|y|Y)$ ]]; then
+#read -r -p "$1 [y/N] " response < /dev/tty
+read -r -p "$1 [y/N] "
+if [[ $REPLY =~ ^(yes|y|Y)$ ]]; then
     echo "Greats ! The upgrade process has started."
     [[ -d $wwwdir ]] || sudo mkdir -p $wwwdir
     [[ -d $installdir ]] || mkdir -p $installdir
     [[ -d /home/pi/Videos ]] || mkdir -p /home/pi/Videos
-    [[ ! -d $unpackdir ]] || rm -r $unpackdir
+    [[ ! -d $unpackdir ]] || rm -rf $unpackdir
     [[ ! -f $downloaddir/RPiMS.zip ]] || rm $downloaddir/RPiMS.zip
+
     curl -sS $repourl -L -o $downloaddir/RPiMS.zip
     unzip  $downloaddir/RPiMS.zip -d $downloaddir
 
-    #sudo cp -R $unpackdir/www/* $wwwdir
-    #sudo cp $unpackdir/RPiMS/* $installdir
-    #sudo chmod u+x $installdir/*.py $installdir/*.sh
+    sudo rm -rf $installdir/*
+    sudo cp $unpackdir/RPiMS/* $installdir
+    sudo chmod u+x $installdir/*.py $installdir/*.sh
 
-    #[[ ! -d $unpackdir ]] || rm -r $unpackdir
-    #[[ ! -f $downloaddir/RPiMS.zip ]] || rm $downloaddir/RPiMS.zip
+    for item in api css js setup
+    do
+	sudo rm -rf $wwwdir/$item
+    done
+    sudo rm $wwwdir/*.*
+
+    for item in api css js setup
+    do
+	sudo cp -R $unpackdir/www/$item $wwwdir
+    done
+
+    sudo cp $unpackdir/www/*.php $wwwdir
+    sudo cp $unpackdir/www/favicon.ico $wwwdir
+
+    [[ ! -d $unpackdir ]] || rm -rf $unpackdir
+    [[ ! -f $downloaddir/RPiMS.zip ]] || rm $downloaddir/RPiMS.zip
 
     echo ""
     echo "-------------------------------------"
@@ -35,7 +51,7 @@ if [[ $response =~ ^(yes|y|Y)$ ]]; then
 fi
 
 echo "Do you want to upgrade the Operating System ?"
-read -r -p "$1 [y/N] " response < /dev/tty
+read -r -p "$1 [y/N] "
 if [[ $response =~ ^(yes|y|Y)$ ]]; then
     echo "Greats ! The upgrade OS has started."
     sudo apt-get -y update
@@ -50,8 +66,8 @@ if [[ $response =~ ^(yes|y|Y)$ ]]; then
 fi
 
 echo "Do you want to upgrade the Python library ?"
-read -r -p "$1 [y/N] " response < /dev/tty
-if [[ $response =~ ^(yes|y|Y)$ ]]; then
+read -r -p "$1 [y/N] "
+if [[ $REPLY =~ ^(yes|y|Y)$ ]]; then
     echo "Greats ! The upgrade OS has started."
 
     sudo python3 -m pip install --upgrade pip setuptools wheel
@@ -70,9 +86,9 @@ echo "Do you want to reboot RPiMS now ?"
 echo ""
 echo "After restarting open http://$_IP/setup or http://127.0.0.1 to configure RPiMS"
 
-read -r -p "$1 [y/N] " response < /dev/tty
+read -r -p "$1 [y/N] "
 
-if [[ $response =~ ^(yes|y|Y)$ ]]; then
+if [[ $REPLY =~ ^(yes|y|Y)$ ]]; then
     sudo reboot
 else
     echo ""
