f24bcfd48bacaf2be8deb54fffdda835e30e59fd
Dariusz Kowalczyk <darton@users.noreply.github.com>
2022-04-26 10:38:19 +0200

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -54,7 +54,7 @@ read -r -p "$1 [y/N] " response < /dev/tty
 if [[ $response =~ ^(yes|y|Y)$ ]]; then
     echo "Greats ! The upgrade OS has started."
 
-    sudo -H pip3 install --upgrade RPi.bme280 smbus2 w1thermsensor redis hiredis pid PyYAML luma.oled luma.lcd adafruit-circuitpython-ads1x15 rshell pyusb
+    sudo -H pip3 install --upgrade RPi.bme280 smbus2 w1thermsensor redis hiredis pid PyYAML luma.oled luma.lcd  luma.core adafruit-circuitpython-ads1x15 rshell pyusb
 
     echo ""
     echo "-------------------------------------"
