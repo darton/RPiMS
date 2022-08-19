69099b457bfac780f2d8516f0938947740110fed
Dariusz Kowalczyk <dariusz@kowalczyk.it>
2022-08-19 12:51:10 +0200

    update

--- install.sh
+++ install.sh
@@ -168,7 +168,7 @@ $PIP3_INSTALL_CMD adafruit-circuitpython-dht
 $INSTALL_CMD libgpiod2 libgpiod-dev
 
 rm $downloaddir/RPiMS.zip
-rmdir $unpackdir
+rm -rf $unpackdir
 
 hostnamectl set-hostname rpims.example.com
 echo  "127.0.1.2       rpims.example.com" | tee -a /etc/hosts
