4268a67996e4bb63baedaf40417f85ee94c95633
Dariusz Kowalczyk <darton@users.noreply.github.com>
2022-08-19 09:41:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -78,10 +78,10 @@ $INSTALL_CMD libfreetype6-dev
 $INSTALL_CMD libopenjp2-7
 $INSTALL_CMD libtiff5
 $INSTALL_CMD libjpeg-dev
-$INSTALL_CMD vlc
-$INSTALL_CMD ffmpeg
-$INSTALL_CMD gpac
-$INSTALL_CMD fbi
+#$INSTALL_CMD vlc
+#$INSTALL_CMD ffmpeg
+#$INSTALL_CMD gpac
+#$INSTALL_CMD fbi
 
 $INSTALL_CMD build-essential
 $INSTALL_CMD python3-gpiozero
