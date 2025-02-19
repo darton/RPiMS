22e3bf7d3329f9e1133770c6a75cfe0745f03cda
Dariusz Kowalczyk <darton@users.noreply.github.com>
2025-02-19 13:40:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -63,20 +63,6 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y autoremove
 
-echo /opt/vc/lib/ | sudo tee /etc/ld.so.conf.d/vc.conf
-ldconfig
-
-$INSTALL_CMD uv4l 
-$INSTALL_CMD uv4l-raspicam
-$INSTALL_CMD uv4l-raspicam-extras
-$INSTALL_CMD uv4l-server
-#$INSTALL_CMD uv4l-server uv4l-uvc uv4l-xscreen uv4l-mjpegstream uv4l-dummy uv4l-raspidisp
-#$INSTALL_CMD uv4l-webrtc
-#$INSTALL_CMD uv4l-webrtc-armv6
-mv -f /etc/uv4l/uv4l-raspicam.conf /etc/uv4l/uv4l-raspicam.conf.org
-ln -s /var/www/html/conf/uv4l-raspicam.conf /etc/uv4l/uv4l-raspicam.conf
-systemctl enable uv4l_raspicam
-
 $INSTALL_CMD git
 $INSTALL_CMD liblockfile-bin
 $INSTALL_CMD libjpeg-dev
@@ -100,6 +86,19 @@ $INSTALL_CMD python3-systemd
 $INSTALL_CMD python3-pip
 $INSTALL_CMD python3-setuptools
 $INSTALL_CMD python3-wheel
+$INSTALL_CMD gunicorn
+$INSTALL_CMD redis-server
+$INSTALL_CMD apache2-utils
+$INSTALL_CMD nginx
+$INSTALL_CMD uv4l 
+$INSTALL_CMD uv4l-raspicam
+$INSTALL_CMD uv4l-raspicam-extras
+$INSTALL_CMD uv4l-server
+#$INSTALL_CMD uv4l-server uv4l-uvc uv4l-xscreen uv4l-mjpegstream uv4l-dummy uv4l-raspidisp
+#$INSTALL_CMD uv4l-webrtc
+#$INSTALL_CMD uv4l-webrtc-armv6
+#for DHT sensors
+$INSTALL_CMD libgpiod2 libgpiod-dev
 
 $PIP3_INSTALL_CMD RPi.bme280 smbus2
 $PIP3_INSTALL_CMD w1thermsensor
@@ -112,10 +111,10 @@ $PIP3_INSTALL_CMD automationhat
 $PIP3_INSTALL_CMD rshell
 $PIP3_INSTALL_CMD pyusb
 $PIP3_INSTALL_CMD setproctitle
+$PIP3_INSTALL_CMD flask
 
 #for DHT sensors
 $PIP3_INSTALL_CMD adafruit-circuitpython-dht
-$INSTALL_CMD libgpiod2 libgpiod-dev
 
 # for Seedstudio Grove Devices 
 cd /tmp
@@ -124,7 +123,7 @@ cd grove.py
 sudo pip3 install .
 rm -rf /tmp/grove.py
 
-$INSTALL_CMD redis-server
+
 sysctl -w vm.overcommit_memory=1
 sysctl -w net.core.somaxconn=512
 echo 'vm.overcommit_memory=1' | tee -a /etc/sysctl.conf
@@ -132,18 +131,12 @@ echo 'net.core.somaxconn=512' | tee -a /etc/sysctl.conf
 echo 'maxmemory 100mb' | tee -a /etc/redis/redis.conf
 systemctl enable redis-server.service
 
-$INSTALL_CMD apache2-utils
-$INSTALL_CMD nginx
-rm $wwwdir/index.nginx-debian.html
 
+rm $wwwdir/index.nginx-debian.html
 mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.org
 cp $unpackdir/etc/nginx-default /etc/nginx/sites-available/default
 cp $unpackdir/etc/nginx.conf /etc/nginx
 chown root.root /etc/nginx/nginx.conf
-
-$INSTALL_CMD gunicorn
-$PIP3_INSTALL_CMD flask
-
 chown -R pi.www-data /var/www
 systemctl enable nginx
 
@@ -181,6 +174,13 @@ systemctl enable rpims.service
 systemctl enable rpims-watcher.path
 systemctl enable rpims-watcher.service
 systemctl enable gunicorn.service
+
+mv -f /etc/uv4l/uv4l-raspicam.conf /etc/uv4l/uv4l-raspicam.conf.org
+ln -s /var/www/html/conf/uv4l-raspicam.conf /etc/uv4l/uv4l-raspicam.conf
+systemctl enable uv4l_raspicam
+
+echo /opt/vc/lib/ | sudo tee /etc/ld.so.conf.d/vc.conf
+ldconfig
 
 rm $downloaddir/RPiMS.zip
 rm -rf $unpackdir
