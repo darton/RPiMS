b64e8f72b60806dcceeaf4b30bd04321d92156c0
Dariusz Kowalczyk <dariusz@kowalczyk.it>
2026-03-14 11:20:32 +0100

    Code refactoring

--- RPiMS/scripts/mediamtx_recording.sh
+++ RPiMS/scripts/mediamtx_recording.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+#
+#  Author : Dariusz Kowalczyk
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License Version 2 as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 
 SCRIPT_NAME="$(basename "$0")"
 URL="http://localhost:9997/v3/config/paths"
