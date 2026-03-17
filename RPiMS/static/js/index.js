73f4fb0f9de483b55460efcd14a55b11300df6e8
Dariusz Kowalczyk <dariusz@kowalczyk.it>
2026-03-17 17:49:11 +0100

    index.js rewritten in plain js - optimized version

--- RPiMS/static/js/index.js
+++ RPiMS/static/js/index.js
@@ -1,277 +1,261 @@
-const DOOR_STATE = {
-    0: "close",
-    1: "open"
+// --- Helpers -------------------------------------------------------------
+
+const qs = sel => document.querySelector(sel);
+const qsa = sel => document.querySelectorAll(sel);
+
+const getEl = el => (typeof el === "string" ? qs(el) : el) || null;
+
+const show = el => {
+    el = getEl(el);
+    if (el) el.style.display = "";
 };
 
-const MOTION_STATE = {
-    0: "nomotion",
-    1: "motion"
+const hide = el => {
+    el = getEl(el);
+    if (el) el.style.display = "none";
 };
+
+const html = (el, v) => {
+    el = getEl(el);
+    if (el) el.innerHTML = v;
+};
+
+const toggle = (el, cond) => cond ? show(el) : hide(el);
+
+const roundPrecised = (n, p) => Math.round(n * 10 ** p) / 10 ** p;
+
+function setGaugeValue(gauge, value, divisor, unit) {
+    gauge = getEl(gauge);
+    if (!gauge || value < 0 || value > 1) return;
+    const angle = value / 2;
+    const display = roundPrecised(value * divisor, 2);
+    const fill = gauge.querySelector(".gauge__fill");
+    const cover = gauge.querySelector(".gauge__cover");
+    if (fill) fill.style.transform = `rotate(${angle}turn)`;
+    if (cover) cover.textContent = `${display} ${unit}`;
+}
+
+// --- Camera --------------------------------------------------------------
 
 let lastUsePicamera = null;
 
-function qs(sel) {
-    return document.querySelector(sel);
-}
-
-function qsa(sel) {
-    return document.querySelectorAll(sel);
-}
-
-function show(el) {
-    if (typeof el === "string") el = qs(el);
-    if (el) el.style.display = "";
-}
-
-function hide(el) {
-    if (typeof el === "string") el = qs(el);
-    if (el) el.style.display = "none";
-}
-
-function html(el, value) {
-    if (typeof el === "string") el = qs(el);
-    if (el) el.innerHTML = value;
-}
-
 function updateCamera(data) {
-    const usePicamera = data.config.setup.use_picamera;
-
-    if (usePicamera === lastUsePicamera) return;
-    lastUsePicamera = usePicamera;
+    const use = data.config.setup.use_picamera;
+    if (use === lastUsePicamera) return;
+    lastUsePicamera = use;
 
     const iframe = qs("#rpicamera iframe");
-
-    if (usePicamera) {
-        iframe.src = "http://" + window.location.hostname + ":8889/cam";
-        show("#rpicamera");
-    } else {
-        iframe.src = "";
-        hide("#rpicamera");
-    }
+    iframe.src = use ? `http://${location.hostname}:8889/cam` : "";
+    toggle("#rpicamera", use);
 }
+
+// --- Weather Station -----------------------------------------------------
 
 function updateWheatherStation(data) {
-    const useWS = data.config.setup.use_weather_station;
-    const windSpeed = data.config.sensors.WEATHER.WIND.SPEED.use;
-    const windDir = data.config.sensors.WEATHER.WIND.DIRECTION.use;
-    const rainfall = data.config.sensors.WEATHER.RAINFALL.use;
+    const cfg = data.config.sensors.WEATHER;
+    const ws = data.sensors.weather_station;
 
-    if (useWS && (windSpeed || windDir || rainfall)) {
-        show("#weather_station");
+    const use = data.config.setup.use_weather_station &&
+                (cfg.WIND.SPEED.use || cfg.WIND.DIRECTION.use || cfg.RAINFALL.use);
 
-        if (windSpeed) {
-            show("#div_wind_speed");
-            html("#wind_speed", data.sensors.weather_station.wind_speed);
-            html("#average_wind_speed", data.sensors.weather_station.average_wind_speed);
-            html("#wind_gust", data.sensors.weather_station.wind_gust);
-            html("#daily_wind_gust", data.sensors.weather_station.daily_wind_gust);
-            html("#daily_average_wind_speed", data.sensors.weather_station.daily_average_wind_speed);
+    toggle("#weather_station", use);
+    if (!use) return;
 
-            const gws = qs("#gws");
-            const gwg = qs("#gwg");
-            const gwg24h = qs("#gwg24h");
+    // Wind speed
+    toggle("#div_wind_speed", cfg.WIND.SPEED.use);
+    if (cfg.WIND.SPEED.use) {
+        html("#wind_speed", ws.wind_speed);
+        html("#average_wind_speed", ws.average_wind_speed);
+        html("#wind_gust", ws.wind_gust);
+        html("#daily_wind_gust", ws.daily_wind_gust);
+        html("#daily_average_wind_speed", ws.daily_average_wind_speed);
 
-            setGaugeValue(gws, Math.round(data.sensors.weather_station.wind_speed) / 100, 100, "km/h");
-            setGaugeValue(gwg, Math.round(data.sensors.weather_station.wind_gust) / 100, 100, "km/h");
-            setGaugeValue(gwg24h, Math.round(data.sensors.weather_station.daily_wind_gust) / 100, 100, "km/h");
-        } else hide("#div_wind_speed");
+        setGaugeValue(qs("#gws"), Math.round(ws.wind_speed) / 100, 100, "km/h");
+        setGaugeValue(qs("#gwg"), Math.round(ws.wind_gust) / 100, 100, "km/h");
+        setGaugeValue(qs("#gwg24h"), Math.round(ws.daily_wind_gust) / 100, 100, "km/h");
+    }
 
-        if (windDir) {
-            show("#wind_direction");
-            html("#average_wind_direction", data.sensors.weather_station.average_wind_direction);
-        } else hide("#wind_direction");
+    // Wind direction
+    toggle("#wind_direction", cfg.WIND.DIRECTION.use);
+    if (cfg.WIND.DIRECTION.use) {
+        html("#average_wind_direction", ws.average_wind_direction);
+    }
 
-        if (rainfall) {
-            show("#raifall");
-            html("#daily_rainfall", data.sensors.weather_station.daily_rainfall);
-        } else hide("#rainfall");
-
-    } else {
-        hide("#weather_station");
+    // Rainfall
+    toggle("#rainfall", cfg.RAINFALL.use);
+    if (cfg.RAINFALL.use) {
+        html("#daily_rainfall", ws.daily_rainfall);
     }
 }
 
+// --- DS18B20 v2 ----------------------------------------------------------
+
 function updateds18b20v2(data) {
-    if (data.config.setup.use_ds18b20_sensor && data.sensors.one_wire) {
-        const container = qs("#ds18b20_container");
-        container.innerHTML = "";
+    if (!data.config.setup.use_ds18b20_sensor || !data.sensors.one_wire) return;
 
-        const addresses = data.config.sensors.ONE_WIRE.DS18B20.addresses;
-        const values = data.sensors.one_wire.ds18b20;
+    const container = qs("#ds18b20_container");
+    container.innerHTML = "";
 
-        const keys = Object.keys(addresses);
-        if (keys.length === 0) {
-            container.insertAdjacentHTML("beforeend",
-                "<div class='sensors ds18b20_sensors'><div class='w3-container w3-margin w3-text-grey'>No DS18B20 sensors detected !</div></div>"
-            );
-            return;
-        }
+    const addresses = data.config.sensors.ONE_WIRE.DS18B20.addresses;
+    const values = data.sensors.one_wire.ds18b20;
 
-        keys.forEach(addr => {
-            const name = addresses[addr]?.name || addr;
-            const temp = values?.[addr] ?? "--";
+    const keys = Object.keys(addresses);
+    if (!keys.length) {
+        container.insertAdjacentHTML("beforeend",
+            `<div class='sensors ds18b20_sensors'>
+                <div class='w3-container w3-margin w3-text-grey'>
+                    No DS18B20 sensors detected !
+                </div>
+            </div>`
+        );
+        return;
+    }
 
-            const htmlBlock = `
-                <div class='sensors ds18b20_sensors'>
-                    <div class='w3-container w3-margin'>
-                        <b>${name}</b> (${addr})
-                        <div style='width: 100%; display: table;'>
-                            <div style='display: table-row'>
-                                <div class='gauge_ds18b20' id='DS18B20_${addr}'>
-                                    <div class='gauge__body'>
-                                        <div class='gauge__fill'></div>
-                                        <div class='gauge__cover'></div>
-                                    </div>
+    keys.forEach(addr => {
+        const name = addresses[addr]?.name || addr;
+        const temp = values?.[addr] ?? "--";
+
+        container.insertAdjacentHTML("beforeend", `
+            <div class='sensors ds18b20_sensors'>
+                <div class='w3-container w3-margin'>
+                    <b>${name}</b> (${addr})
+                    <div style='width: 100%; display: table;'>
+                        <div style='display: table-row'>
+                            <div class='gauge_ds18b20' id='DS18B20_${addr}'>
+                                <div class='gauge__body'>
+                                    <div class='gauge__fill'></div>
+                                    <div class='gauge__cover'></div>
                                 </div>
                             </div>
                         </div>
-                        <div class='ds18b20_value'>${temp} °C</div>
                     </div>
+                    <div class='ds18b20_value'>${temp} °C</div>
                 </div>
-            `;
-            container.insertAdjacentHTML("beforeend", htmlBlock);
-        });
-    }
+            </div>
+        `);
+    });
 }
+
+// --- DS18B20 gauges ------------------------------------------------------
 
 function updateDS18b20(data) {
-    if (data.config.setup.use_ds18b20_sensor && data.sensors.one_wire) {
-        qsa(".ds18b20_sensors").forEach(el => show(el));
+    const use = data.config.setup.use_ds18b20_sensor && data.sensors.one_wire;
+    qsa(".ds18b20_sensors").forEach(el => toggle(el, use));
+    if (!use) return;
 
-        const values = {};
-        for (let id in data.sensors.one_wire.ds18b20) {
-            values[id] = roundPrecised(data.sensors.one_wire.ds18b20[id].temperature, 1);
+    const sensors = data.sensors.one_wire.ds18b20;
+
+    Object.entries(sensors).forEach(([id, obj]) => {
+        const gauge = qs(`#DS18B20_${id}`);
+        const temp = roundPrecised(obj.temperature, 1);
+
+        if (temp) {
+            show(gauge);
+            setGaugeValue(gauge, temp / 100, 100, "°C");
+        } else {
+            hide(gauge);
+            setGaugeValue(gauge, 0, 1, "");
         }
-
-        for (let id in values) {
-            const el = qs("#DS18B20_" + id);
-            const val = values[id];
-
-            if (val) {
-                show(el);
-                setGaugeValue(el, val / 100, 100, "°C");
-            } else {
-                hide(el);
-                setGaugeValue(el, "NULL", "", "");
-            }
-        }
-    } else {
-        qsa(".ds18b20_sensors").forEach(el => hide(el));
-    }
+    });
 }
+
+// --- Contact sensors -----------------------------------------------------
 
 function updateContactSensors(data) {
-    if (data.config.setup.use_contact_sensor) {
-        show("#contact_sensors");
+    const use = data.config.setup.use_contact_sensor;
+    toggle("#contact_sensors", use);
+    if (!use) return;
 
-        for (let key in data.sensors.contact_sensors) {
-            const value = Number(data.sensors.contact_sensors[key]);
-            const el = qs("#" + key);
-
-            html(el, value === 1 ? "open - 1" : "close - 0");
-
-            el.classList.toggle("alarm", value === 1);
-            el.classList.toggle("value", value === 0);
-        }
-    } else hide("#contact_sensors");
+    Object.entries(data.sensors.contact_sensors).forEach(([id, val]) => {
+        const el = qs("#" + id);
+        const num = Number(val);
+        html(el, num ? "open - 1" : "close - 0");
+        el.classList.toggle("alarm", num === 1);
+        el.classList.toggle("value", num === 0);
+    });
 }
+
+// --- Digital sensors -----------------------------------------------------
 
 function updateDigitalSensors(data) {
-    if (data.config.setup.use_digital_sensor) {
-        show("#digital_sensors");
+    const use = data.config.setup.use_digital_sensor;
+    toggle("#digital_sensors", use);
+    if (!use) return;
 
-        for (let key in data.sensors.digital_sensors) {
-            const value = Number(data.sensors.digital_sensors[key]);
-            const el = qs("#" + key);
-
-            html(el, value === 1 ? "motion - 1" : "nomotion - 0");
-
-            el.classList.toggle("alarm", value === 1);
-            el.classList.toggle("value", value === 0);
-        }
-    } else hide("#digital_sensors");
+    Object.entries(data.sensors.digital_sensors).forEach(([id, val]) => {
+        const el = qs("#" + id);
+        const num = Number(val);
+        html(el, num ? "motion - 1" : "nomotion - 0");
+        el.classList.toggle("alarm", num === 1);
+        el.classList.toggle("value", num === 0);
+    });
 }
+
+// --- DHT -----------------------------------------------------------------
 
 function updateDHTSensor(data) {
-    if (data.config.setup.use_dht_sensor) {
-        show("#dht_sensor");
+    const use = data.config.setup.use_dht_sensor;
+    toggle("#dht_sensor", use);
+    if (!use) return;
 
-        const t = roundPrecised(data.sensors.dht.temperature, 1);
-        const h = Math.round(data.sensors.dht.humidity);
+    const t = roundPrecised(data.sensors.dht.temperature, 1);
+    const h = Math.round(data.sensors.dht.humidity);
 
-        setGaugeValue(qs("#gdhttemp"), t / 100, 100, "°C");
-        setGaugeValue(qs("#gdhthum"), h / 100, 100, "%");
-    } else hide("#dht_sensor");
+    setGaugeValue(qs("#gdhttemp"), t / 100, 100, "°C");
+    setGaugeValue(qs("#gdhthum"), h / 100, 100, "%");
 }
+
+// --- BME280 --------------------------------------------------------------
 
 function updateBME280Sensor(data) {
-    const use = data.config.setup.use_bme280_sensor;
-    const use1 = data.config.sensors.BME280.id1.use;
-    const use2 = data.config.sensors.BME280.id2.use;
-    const use3 = data.config.sensors.BME280.id3.use;
+    const cfg = data.config.sensors.BME280;
+    const use = data.config.setup.use_bme280_sensor &&
+                (cfg.id1.use || cfg.id2.use || cfg.id3.use);
 
-    function read(id) {
-        const sensor = data.sensors.bme280?.[id];
-        if (!sensor) return;
+    toggle("#bme280_sensors", use);
+    if (!use) return;
 
-        const t = roundPrecised(sensor.temperature, 1);
-        const h = Math.round(sensor.humidity);
-        const p = Math.round(sensor.pressure);
+    const read = id => {
+        const s = data.sensors.bme280?.[id];
+        if (!s) return;
 
-        html("#" + id + "_BME280_name", sensor.name);
+        html(`#${id}_BME280_name`, s.name);
 
-        setGaugeValue(qs(`#${id}_BME280_Temperature`), t / 100, 100, "°C");
-        setGaugeValue(qs(`#${id}_BME280_Humidity`), h / 100, 100, "%");
-        setGaugeValue(qs(`#${id}_BME280_Pressure`), p / 1100, 1100, "hPa");
-    }
+        setGaugeValue(qs(`#${id}_BME280_Temperature`), roundPrecised(s.temperature, 1) / 100, 100, "°C");
+        setGaugeValue(qs(`#${id}_BME280_Humidity`), Math.round(s.humidity) / 100, 100, "%");
+        setGaugeValue(qs(`#${id}_BME280_Pressure`), Math.round(s.pressure) / 1100, 1100, "hPa");
+    };
 
-    if (use && (use1 || use2 || use3)) {
-        show("#bme280_sensors");
-
-        if (use1) { read("id1"); show("#id1_BME280"); } else hide("#id1_BME280");
-        if (use2) { read("id2"); show("#id2_BME280"); } else hide("#id2_BME280");
-        if (use3) { read("id3"); show("#id3_BME280"); } else hide("#id3_BME280");
-
-    } else hide("#bme280_sensors");
+    ["id1", "id2", "id3"].forEach(id => {
+        toggle(`#${id}_BME280`, cfg[id].use);
+        if (cfg[id].use) read(id);
+    });
 }
+
+// --- System info ---------------------------------------------------------
 
 function updateSystemInfo(data) {
-    if (data.config.setup.show_sys_info) {
-        show("#sys_info");
-        html("#hostip", data.system.hostip);
-        html("#hostname", data.system.hostname);
-        html("#location", data.system.location);
-    } else hide("#sys_info");
+    const sys = data.system || {};
 
-    if (data.config.setup.use_cpu_sensor) {
-        show("#CPU_Temperature");
+    const showSys = data.config.setup.show_sys_info;
+    toggle("#sys_info", showSys);
+    if (showSys) {
+        html("#hostip", sys.hostip ?? "");
+        html("#hostname", sys.hostname ?? "");
+        html("#location", sys.location ?? "");
+    }
 
-        const temp = data.sensors.cpu.temperature;
-        if (temp) {
-            html("#CPU_Temperature_value", roundPrecised(temp, 0));
-            html("#CPU_Temperature_unit", "°C");
-        } else {
-            html("#CPU_Temperature_value", "NaN");
-            html("#CPU_Temperature_unit", "");
-        }
-    } else hide("#CPU_Temperature");
+    const cpu = data.config.setup.use_cpu_sensor;
+    toggle("#CPU_Temperature", cpu);
+
+    if (cpu) {
+        const t = data.sensors.cpu?.temperature;
+        html("#CPU_Temperature_value", t != null ? roundPrecised(t, 0) : "NaN");
+        html("#CPU_Temperature_unit", t != null ? "°C" : "");
+    }
 }
 
-function roundPrecised(number, precision) {
-    const power = Math.pow(10, precision);
-    return Math.round(number * power) / power;
-}
-
-function setGaugeValue(gauge, value, divisor, unit) {
-    if (!gauge) return;
-    if (value < 0 || value > 1) return;
-
-    const value1 = value / 2;
-    const value2 = roundPrecised(value * divisor, 2);
-
-    gauge.querySelector(".gauge__fill").style.transform = `rotate(${value1}turn)`;
-    gauge.querySelector(".gauge__cover").textContent = `${value2} ${unit}`;
-}
+// --- Fetch loop ----------------------------------------------------------
 
 async function getJsonData() {
     try {
@@ -286,6 +270,7 @@ async function getJsonData() {
         updateDHTSensor(data);
         updateWheatherStation(data);
         updateds18b20v2(data);
+        updateDS18b20(data);
 
     } catch (e) {
         console.error("API error:", e);
