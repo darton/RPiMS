a6b81abe1e59b968e947793400b04eb3b6ac22dc
Dariusz Kowalczyk <dariusz@kowalczyk.it>
2026-03-26 15:38:14 +0100

    Code refactoring

--- RPiMS/scripts/gethostinfo.sh
+++ RPiMS/scripts/gethostinfo.sh
@@ -14,10 +14,10 @@
 
 redis_cmd='timeout 2 /usr/bin/redis-cli'
 
-if [[ $(id -u) -ne 0 ]]; then
-  echo "This script must be executed as root or using sudo"
-  exit 1
-fi
+#if [[ $(id -u) -ne 0 ]]; then
+#  echo "This script must be executed as root or using sudo"
+#  exit 1
+#fi
 
 _IP=$(ip route get 1.1.1.1 | awk '/1.1.1.1/{print $7}')
 
@@ -25,13 +25,13 @@ if [ "$_IP" ]; then
     $redis_cmd set hostip "$_IP" 2>&1 > /dev/null
 fi
 
-_UPTIME=$(/usr/bin/uptime -s)
+_UPTIME=$(/usr/bin/uptime -s|cut -d' ' -f2)
 $redis_cmd set uptime "$_UPTIME" 2>&1 > /dev/null
 
-_FSUSED=$(df -h| awk '/root/ {print $5}')
+_FSUSED=$(df -h| awk '/\/$/ {print $5+0}')
 $redis_cmd set fsused "$_FSUSED" 2>&1 > /dev/null
 
-_MEMUSED=$(free -m |awk '/Mem/ {print $3}')
+_MEMUSED=$(free -m |awk '/Mem/ {print $3/$2*100}')
 $redis_cmd set memused "$_MEMUSED" 2>&1 > /dev/null
 
 exit 0
